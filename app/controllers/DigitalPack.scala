869869d1d3dbf6933601ba7062cd3d2eaad34bcf
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-04-24 11:10:00 +0100

    Handle international variants of Digital Pack template

--- app/controllers/DigitalPack.scala
+++ app/controllers/DigitalPack.scala
@@ -3,10 +3,28 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+case class DigitalEdition(id: String, name: String, price: String, cmp: String)
+
+object DigitalEdition {
+
+  object UK extends DigitalEdition("uk", "UK", "£11.99", "dis_2408")
+  object US extends DigitalEdition("us", "US", "$19.99", "dis_2378")
+  object AU extends DigitalEdition("au", "Australia", "$21.50", "dis_2379")
+
+}
+
 object DigitalPack extends Controller {
 
   def uk = Action {
-    Ok(views.html.digitalpack.uk())
+    Ok(views.html.digitalpack.info(DigitalEdition.UK))
+  }
+
+  def us = Action {
+    Ok(views.html.digitalpack.info(DigitalEdition.US))
+  }
+
+  def au = Action {
+    Ok(views.html.digitalpack.info(DigitalEdition.AU))
   }
 
 }
