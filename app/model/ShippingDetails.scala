cd688f32f7832bf7fadbfc6a185452e4c93f6f55
rtudor <tudorraul@gmail.com>
2015-04-27 17:09:25 +0100

    integrate shipping template

--- app/model/ShippingDetails.scala
+++ app/model/ShippingDetails.scala
@@ -1,9 +1,12 @@
 package model
 
 case class ShippingDetails(name: String,
-                   packageType: String,
-                   method: String,
-                   everyday: Option[String] = None,
-                   sixday: Option[String] = None,
-                   weekend: Option[String] = None,
-                   sunday: Option[String] = None)
+                           packageType: String,
+                           method: String,
+                           everyday: Option[String] = None,
+                           sixday: Option[String] = None,
+                           weekend: Option[String] = None,
+                           sunday: Option[String] = None) {
+  val isCollection = method == "collection"
+  val isPaperDigital = packageType == "paper-digital"
+}
