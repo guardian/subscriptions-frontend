9f8ffdb339484de10847cd5248ea2f6ba0fe10d9
rtudor <tudorraul@gmail.com>
2015-04-28 17:45:32 +0100

    handle selecting package

--- app/controllers/Shipping.scala
+++ app/controllers/Shipping.scala
@@ -1,12 +1,26 @@
 package controllers
 
 import model.ShippingDetails
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{AnyContent, Request, Action, Controller}
 
 object Shipping extends Controller{
 
-  def collectionPaperDigital = Action {
-    Ok(views.html.shipping.index(ShippingDetails(
+  def index(request: Request[AnyContent], shippingDetails: ShippingDetails) =
+    request.body.asFormUrlEncoded
+      .flatMap(_.get("package").map(_.head))
+      .map(_.split("-").last)
+      .collect {
+        case "everyday" => shippingDetails.sixday
+        case "sixday" => shippingDetails.sixday
+        case "weekend" => shippingDetails.weekend
+        case "sunday" => shippingDetails.sunday
+      }
+      .flatten
+      .map(Redirect(_))
+      .getOrElse(Ok(views.html.shipping.index(shippingDetails)))
+
+  def collectionPaperDigital = Action { request =>
+    index(request, ShippingDetails(
       name = "Paper + digital voucher subscription",
       method = "collection",
       packageType = "paper-digital",
@@ -14,22 +28,22 @@ object Shipping extends Controller{
       sixday = Some("https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv6&skip=1"),
       weekend = Some("https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv2&skip=1"),
       sunday = Some("https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=ov1&skip=1")
-    )))
+    ))
   }
 
-  def collectionPaper = Action {
-    Ok(views.html.shipping.index(ShippingDetails(
+  def collectionPaper = Action { request =>
+    index(request, ShippingDetails(
       name = "Paper voucher subscription",
       method = "collection",
       packageType = "paper",
       everyday = Some("https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv7&skip=1"),
       sixday = Some("https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv6&skip=1"),
       weekend = Some("https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv2&skip=1")
-    )))
+    ))
   }
 
-  def deliveryPaperDigital = Action {
-    Ok(views.html.shipping.index(ShippingDetails(
+  def deliveryPaperDigital = Action { request =>
+    index(request, ShippingDetails(
       name = "Paper + digital home delivery subscription",
       method = "delivery",
       packageType = "paper-digital",
@@ -37,18 +51,18 @@ object Shipping extends Controller{
       sixday = Some("https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SIXDAY%2B"),
       weekend = Some("https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=WEEKEND%2B"),
       sunday = Some("https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SUNDAY%2B")
-    )))
+    ))
   }
 
-  def deliveryPaper = Action {
-    Ok(views.html.shipping.index(ShippingDetails(
+  def deliveryPaper = Action { request =>
+    index(request, ShippingDetails(
       name = "Paper home delivery subscription",
       method = "delivery",
       packageType = "paper",
       everyday = Some("https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=EVERYDAY"),
       sixday = Some("https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SIXDAY"),
       weekend = Some("https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=WEEKEND")
-    )))
+    ))
   }
 
 }
