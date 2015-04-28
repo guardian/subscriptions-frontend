d9ee867d73e9ca22dce5b3bf72b54e2426bf0ac3
rtudor <tudorraul@gmail.com>
2015-04-28 16:54:03 +0100

    add international pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,4 +8,11 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
+  def index(edition: DigitalEdition) = Action {
+    Ok(views.html.index_intl(edition))
+  }
+
+  def indexAu() = index(DigitalEdition.AU)
+
+  def indexUs() = index(DigitalEdition.US)
 }
