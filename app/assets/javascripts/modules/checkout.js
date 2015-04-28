0a72400e9ce565073eb21ee2ed9eaf5634525f55
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-04-28 14:49:16 +0100

    Dummy address lookup

--- app/assets/javascripts/modules/checkout.js
+++ app/assets/javascripts/modules/checkout.js
@@ -1,19 +1,53 @@
 define(['bean'], function (bean) {
 
     var IS_HIDDEN = 'is-hidden',
-        MANUAL_ADDRESS_ELEM = document.querySelector('.js-manual-address'),
-        FULL_ADDRESS_ELEM = document.querySelector('.js-full-address');
+        HOUSE_ELEM = document.querySelector('.js-checkout-house'),
+        STREET_ELEM = document.querySelector('.js-checkout-street'),
+        TOWN_ELEM = document.querySelector('.js-checkout-town'),
+        POSTCODE_ELEM = document.querySelector('.js-checkout-postcode'),
+        FIND_ADDRESS_ELEM = document.querySelector('.js-checkout-find-address'),
+        MANUAL_ADDRESS_ELEM = document.querySelector('.js-checkout-manual-address'),
+        FULL_ADDRESS_ELEM = document.querySelector('.js-checkout-full-address');
 
-    var manualAddress = function() {
+    var findAddress = function () {
+        bean.on(FIND_ADDRESS_ELEM, 'click', function (e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            if (POSTCODE_ELEM.value) {
+                // TODO: Ajax to lookup service
+                populateAddressFields({
+                    'house': 'Flat 14 Bankside House',
+                    'street': 'West Hill',
+                    'town': 'Putney'
+                });
+                showFullAddressFields();
+            }
+        });
+    };
+
+    var populateAddressFields = function (address) {
+        HOUSE_ELEM.value = address.house;
+        STREET_ELEM.value = address.street;
+        TOWN_ELEM.value = address.town;
+    };
+
+    var manualAddress = function () {
         bean.on(MANUAL_ADDRESS_ELEM, 'click', function (e) {
             e.preventDefault();
             e.stopImmediatePropagation();
-            FULL_ADDRESS_ELEM.classList.remove(IS_HIDDEN);
+
+            showFullAddressFields();
             MANUAL_ADDRESS_ELEM.classList.add(IS_HIDDEN);
         });
     };
 
+    var showFullAddressFields = function () {
+        FULL_ADDRESS_ELEM.classList.remove(IS_HIDDEN);
+    };
+
     function init() {
+        findAddress();
         manualAddress();
     }
 
