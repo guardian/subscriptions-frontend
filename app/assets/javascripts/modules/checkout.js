4b226a468873d3100c2b486ec5922d0c02ee5786
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-04-29 15:06:47 +0100

    Toggle fieldsets

--- app/assets/javascripts/modules/checkout.js
+++ app/assets/javascripts/modules/checkout.js
@@ -1,30 +1,37 @@
 define(['$', 'bean'], function ($, bean) {
 
-    var $FIRST_NAME             = $('.js-checkout-first'),
-        $LAST_NAME              = $('.js-checkout-last'),
-        $EMAIL                  = $('.js-checkout-email'),
-        $ADDRESS1               = $('.js-checkout-house'),
-        $ADDRESS2               = $('.js-checkout-street'),
-        $ADDRESS3               = $('.js-checkout-town'),
-        $POSTCODE               = $('.js-checkout-postcode'),
-        $ACCOUNT                = $('.js-checkout-account'),
-        $SORTCODE1              = $('.js-checkout-sortcode1'),
-        $SORTCODE2              = $('.js-checkout-sortcode2'),
-        $SORTCODE3              = $('.js-checkout-sortcode3'),
-        $HOLDER                 = $('.js-checkout-holder'),
-        $FIND_ADDRESS           = $('.js-checkout-find-address'),
-        $MANUAL_ADDRESS         = $('.js-checkout-manual-address'),
-        $FULL_ADDRESS           = $('.js-checkout-full-address'),
-        $YOUR_DETAILS_SUBMIT    = $('.js-checkout-your-details-submit'),
-        $PAYMENT_DETAILS_SUBMIT = $('.js-checkout-payment-details-submit'),
-        $REVIEW_NAME            = $('.js-checkout-review-name'),
-        $REVIEW_ADDRESS         = $('.js-checkout-review-address'),
-        $REVIEW_EMAIL           = $('.js-checkout-review-email'),
-        $REVIEW_ACCOUNT         = $('.js-checkout-review-account'),
-        $REVIEW_SORTCODE        = $('.js-checkout-review-sortcode'),
-        $REVIEW_HOLDER          = $('.js-checkout-review-holder'),
-        $SMALLPRINT             = $('.js-checkout-smallprint'),
+    var $FIRST_NAME                 = $('.js-checkout-first'),
+        $LAST_NAME                  = $('.js-checkout-last'),
+        $EMAIL                      = $('.js-checkout-email'),
+        $ADDRESS1                   = $('.js-checkout-house'),
+        $ADDRESS2                   = $('.js-checkout-street'),
+        $ADDRESS3                   = $('.js-checkout-town'),
+        $POSTCODE                   = $('.js-checkout-postcode'),
+        $ACCOUNT                    = $('.js-checkout-account'),
+        $SORTCODE1                  = $('.js-checkout-sortcode1'),
+        $SORTCODE2                  = $('.js-checkout-sortcode2'),
+        $SORTCODE3                  = $('.js-checkout-sortcode3'),
+        $HOLDER                     = $('.js-checkout-holder'),
+        $FIND_ADDRESS               = $('.js-checkout-find-address'),
+        $MANUAL_ADDRESS             = $('.js-checkout-manual-address'),
+        $FULL_ADDRESS               = $('.js-checkout-full-address'),
+        $YOUR_DETAILS_SUBMIT        = $('.js-checkout-your-details-submit'),
+        $PAYMENT_DETAILS_SUBMIT     = $('.js-checkout-payment-details-submit'),
+        $REVIEW_NAME                = $('.js-checkout-review-name'),
+        $REVIEW_ADDRESS             = $('.js-checkout-review-address'),
+        $REVIEW_EMAIL               = $('.js-checkout-review-email'),
+        $REVIEW_ACCOUNT             = $('.js-checkout-review-account'),
+        $REVIEW_SORTCODE            = $('.js-checkout-review-sortcode'),
+        $REVIEW_HOLDER              = $('.js-checkout-review-holder'),
+        $SMALLPRINT                 = $('.js-checkout-smallprint'),
+        $FIELDSET_YOUR_DETAILS      = $('.js-fieldset-your-details'),
+        $FIELDSET_PAYMENT_DETAILS   = $('.js-fieldset-payment-details'),
+        $FIELDSET_REVIEW            = $('.js-fieldset-review'),
+        $EDIT_YOUR_DETAILS          = $('.js-edit-your-details'),
+        $EDIT_PAYMENT_DETAILS       = $('.js-edit-payment-details'),
 
+        FIELDSET_COLLAPSED = 'fieldset--collapsed',
+        FIELDSET_COMPLETE = 'data-fieldset-complete',
         IS_HIDDEN = 'is-hidden';
 
     var findAddress = function () {
@@ -61,6 +68,49 @@ define(['$', 'bean'], function ($, bean) {
         $FULL_ADDRESS.removeClass(IS_HIDDEN);
     };
 
+    var toggleFieldsets = function () {
+        bean.on($YOUR_DETAILS_SUBMIT[0], 'click', function (e) {
+            e.preventDefault();
+            $FIELDSET_YOUR_DETAILS.addClass(FIELDSET_COLLAPSED).attr(FIELDSET_COMPLETE, '');
+            $FIELDSET_PAYMENT_DETAILS.removeClass(FIELDSET_COLLAPSED);
+            $EDIT_YOUR_DETAILS.removeClass(IS_HIDDEN);
+            $EDIT_PAYMENT_DETAILS.addClass(IS_HIDDEN);
+        });
+
+        bean.on($PAYMENT_DETAILS_SUBMIT[0], 'click', function (e) {
+            e.preventDefault();
+            $FIELDSET_PAYMENT_DETAILS.addClass(FIELDSET_COLLAPSED).attr(FIELDSET_COMPLETE, '');
+            $FIELDSET_REVIEW.removeClass(FIELDSET_COLLAPSED);
+            $EDIT_PAYMENT_DETAILS.removeClass(IS_HIDDEN);
+        });
+
+        bean.on($EDIT_YOUR_DETAILS[0], 'click', function (e) {
+            e.preventDefault();
+            collapseFieldsets();
+            $FIELDSET_YOUR_DETAILS.removeClass(FIELDSET_COLLAPSED);
+            $EDIT_YOUR_DETAILS.addClass(IS_HIDDEN);
+            if ($FIELDSET_PAYMENT_DETAILS.attr(FIELDSET_COMPLETE) !== null) {
+                $EDIT_PAYMENT_DETAILS.removeClass(IS_HIDDEN);
+            } else {
+                $EDIT_PAYMENT_DETAILS.addClass(IS_HIDDEN);
+            }
+        });
+
+        bean.on($EDIT_PAYMENT_DETAILS[0], 'click', function (e) {
+            e.preventDefault();
+            collapseFieldsets();
+            $FIELDSET_PAYMENT_DETAILS.removeClass(FIELDSET_COLLAPSED);
+            $EDIT_PAYMENT_DETAILS.addClass(IS_HIDDEN);
+            $EDIT_YOUR_DETAILS.removeClass(IS_HIDDEN);
+        });
+    };
+
+    var collapseFieldsets = function () {
+        $FIELDSET_YOUR_DETAILS.addClass(FIELDSET_COLLAPSED);
+        $FIELDSET_PAYMENT_DETAILS.addClass(FIELDSET_COLLAPSED);
+        $FIELDSET_REVIEW.addClass(FIELDSET_COLLAPSED);
+    };
+
     var reviewDetails = function () {
         bean.on($YOUR_DETAILS_SUBMIT[0], 'click', function (e) {
             e.preventDefault();
@@ -81,6 +131,7 @@ define(['$', 'bean'], function ($, bean) {
     function init() {
         findAddress();
         manualAddress();
+        toggleFieldsets();
         reviewDetails();
     }
 
