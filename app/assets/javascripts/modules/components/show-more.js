937b1fe1fdebe97dfef58f6e72e1772990253045
rtudor <tudorraul@gmail.com>
2015-05-18 16:06:52 +0100

    add show more buttons

---
+++ app/assets/javascripts/modules/components/show-more.js
@@ -0,0 +1,18 @@
+define(['$', 'bonzo', 'bean'], function ($, bonzo, bean) {
+
+    function init() {
+        $('.js-show-more-button').each(function (btn) {
+            bean.on(btn, 'click', function (e) {
+                bonzo(btn).toggleClass('button--show-more button--show-less');
+                $('.js-' + bonzo(btn).attr('data-toggle-set')).each(function(elem){
+                    bonzo(elem).toggleClass('visible');
+                });
+            });
+        })
+    }
+
+    return {
+        init: init
+    };
+
+});
---
+++ app/assets/javascripts/modules/digitalpack.js
@@ -0,0 +1,11 @@
+define(['modules/components/show-more'], function (showMore) {
+
+    function init() {
+        showMore.init()
+    }
+
+    return {
+        init: init
+    };
+
+});
