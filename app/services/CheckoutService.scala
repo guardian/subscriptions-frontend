410e35560e09e574d89ef8885ac9abb3a86e8ce2
rtudor <tudorraul@gmail.com>
2015-06-01 16:27:06 +0100

    add Salesforce integration

--- app/services/CheckoutService.scala
+++ app/services/CheckoutService.scala
@@ -1,31 +1,51 @@
 package services
 
+import com.typesafe.scalalogging.LazyLogging
 import model.SubscriptionData
 import services.IdentityService.IdUser
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.Left
 
-object CheckoutService {
+object CheckoutService extends LazyLogging {
 
   sealed class CheckoutException extends RuntimeException
   object LoginRequired extends CheckoutException
   object InvalidLoginCookie extends CheckoutException
+  object SalesforceUserNotCreated extends CheckoutException
 
   def processSubscription(subscriptionData: SubscriptionData, scGuUCookie:Option[String] = None): Future[Either[CheckoutException,String]] = {
 
-    val userId: Future[Either[CheckoutException, IdUser]] = scGuUCookie.map {
-      IdentityService.userLookupByScGuU(_)
-        .map(_.toRight(InvalidLoginCookie))
-    }.orElse {
-      Some(IdentityService.userLookupByEmail(subscriptionData.personalData.email)
-        .map(_.toRight(LoginRequired)))
-    }.get
+    def eventualFailureReason = (ex: CheckoutException) => Future.successful(Left(ex))
+
+    val lookupUser: Future[Either[CheckoutException, IdUser]] =
+      scGuUCookie.map(
+        IdentityService.userLookupByScGuU(_)
+          .map(_.toRight(InvalidLoginCookie)))
+        .orElse(
+          Some(IdentityService.userLookupByEmail(subscriptionData.personalData.email)
+            .map(_.toRight(LoginRequired))))
+        .get
+
+    lookupUser.onComplete{case x => println(s">>> lookupUser: $x")}
+
+    def createSFUser = (idUser:Either[CheckoutException, IdUser]) => {
+      println(s">>> createSFUser start")
+      val x = idUser.fold(eventualFailureReason, SalesforceService.createSFUser(subscriptionData.personalData, _))
+      println(s">>> createSFUser end")
+      x
+    }
 
     for {
-      userId <- userId 
-    } yield println(s">>>userID:$userId")
+      idUser <- lookupUser
+      sfUser <- createSFUser(idUser)
+    } yield {
+      println(s">>>userID:$idUser")
+      println(s">>>sfUser:$sfUser")
+    }
 
     Future.successful(Right("AB123456"))
   }
+
 }
---
+++ app/services/SalesforceService.scala
@@ -0,0 +1,112 @@
+package services
+
+import akka.agent.Agent
+import com.gu.membership.salesforce.Member.Keys
+import com.gu.membership.salesforce.{Authentication, MemberId, MemberRepository, Scalaforce}
+import com.typesafe.scalalogging.LazyLogging
+import configuration.Config
+import model.PersonalData
+import play.api.Logger
+import play.api.libs.concurrent.Akka
+import play.api.libs.json.{JsObject, Json}
+import services.CheckoutService._
+import services.IdentityService.IdUser
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{FiniteDuration, _}
+import scala.concurrent.{Await, Future}
+import scala.util.Left
+
+object SalesforceService extends LazyLogging {
+
+  def createSFUser(personalData: PersonalData, idUser: IdUser): Future[Either[CheckoutException, MemberId]] = {
+    val data: JsObject = createSalesforceUserData(personalData)
+    println("Hi again!")
+    val x = SalesforceRepo.upsert(idUser.id, data)
+      .map(Right(_)).recoverWith {
+        case e: Throwable =>
+          println("Could not create saleforce user: "+ e.getMessage)
+          logger.error("Could not create saleforce user", e)
+          Future.successful(Left(SalesforceUserNotCreated))
+      }
+    x.onSuccess{case x => println(s">>> createSFUser: $x")}
+    x.onFailure{case x => println(s">>> createSFUser: $x")}
+    x
+  }
+
+  private def createSalesforceUserData(personalData: PersonalData): JsObject = {
+    Seq(Json.obj(
+      Keys.EMAIL -> personalData.email,
+      Keys.FIRST_NAME -> personalData.firstName,
+      Keys.LAST_NAME -> personalData.lastName,
+      Keys.MAILING_STREET -> personalData.address.street,
+      Keys.MAILING_CITY -> personalData.address.town,
+      //      Keys.MAILING_STATE -> ???,
+      Keys.MAILING_POSTCODE -> personalData.address.postcode,
+      Keys.MAILING_COUNTRY -> "United Kingdom",
+      Keys.ALLOW_MEMBERSHIP_MAIL -> true
+    )) ++ Map(
+      Keys.ALLOW_THIRD_PARTY_EMAIL -> Some(true),
+      Keys.ALLOW_GU_RELATED_MAIL -> Some(true)
+    ).collect { case (k, Some(v)) => Json.obj(k -> v) }
+  }.reduce(_ ++ _)
+
+}
+
+object SalesforceRepo extends MemberRepository {
+  override val salesforce = new Scalaforce {
+    val authTask = ScheduledTask("", Authentication("", ""), 0.seconds, 30.minutes)(getAuthentication)
+
+    def authentication: Authentication = authTask.get()
+    println(">>>cfg: "+Config.Salesforce)
+
+    override val consumerKey = Config.Salesforce.consumerKey
+    override val apiUsername = Config.Salesforce.apiUsername
+    override val consumerSecret = Config.Salesforce.consumerSecret
+    override val apiToken = Config.Salesforce.apiToken
+    override val apiPassword = Config.Salesforce.apiPassword
+    override val application = "subscriptions-frontend"
+    override val apiURL = Config.Salesforce.apiURL.toString
+    override val stage = Config.Salesforce.envName
+  }
+}
+
+trait ScheduledTask[T] {
+  import play.api.Play.current
+
+  val initialValue: T
+  val initialDelay: FiniteDuration
+  val interval: FiniteDuration
+
+  val name = getClass.getSimpleName
+
+  private implicit val system = Akka.system
+  lazy val agent = Agent[T](initialValue)
+
+  def refresh(): Future[T]
+
+  def start() {
+    Logger.debug(s"Starting $name scheduled task")
+    system.scheduler.schedule(initialDelay, interval) {
+      agent.sendOff { _ =>
+        Logger.debug(s"Refreshing $name scheduled task")
+        Await.result(refresh(), 25.seconds)
+      }
+    }
+  }
+
+  def get() = agent.get()
+}
+
+object ScheduledTask {
+  def apply[T](taskName: String, initValue: T, initDelay: FiniteDuration, intervalPeriod: FiniteDuration)(refresher: => Future[T]) =
+    new ScheduledTask[T] {
+      val initialValue = initValue
+      val initialDelay = initDelay
+      val interval = intervalPeriod
+
+      override val name = taskName
+
+      def refresh(): Future[T] = refresher
+    }
+}
