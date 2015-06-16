be0875978084f1243c6de524f13ece4630d94bcd
Andrea Fiore <andrea.fiore@composable.io>
2015-06-16 17:59:25 +0100

    Log a warning if conf/assets.map cannot be found

--- app/utils/Asset.scala
+++ app/utils/Asset.scala
@@ -1,14 +1,19 @@
 package utils
 
+import play.api.Logger
 import play.api.libs.json.{JsObject, Json}
-
 import scala.io.Source
 
 object Asset {
-  lazy val map = {
+  private val logger = Logger(this.getClass)
+
+  val map: Map[String, String] = {
     val resourceOpt = Option(getClass.getClassLoader.getResourceAsStream("assets.map"))
     val jsonOpt = resourceOpt.map(Source.fromInputStream(_).mkString).map(Json.parse)
-    jsonOpt.map(_.as[JsObject].fields.toMap.mapValues(_.as[String])).getOrElse(Map.empty)
+    jsonOpt.map(_.as[JsObject].fields.toMap.mapValues(_.as[String])).getOrElse {
+      logger.warn("Could not find a config/assets.map file. You can generate it by running 'grunt compile'")
+      Map.empty
+    }
   }
 
   def at(path: String) = "/assets/" + map.getOrElse(path, path)
