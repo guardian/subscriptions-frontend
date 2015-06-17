a637665eb6931412cb1b2fc9eca8e2e8a71c318e
Andrea Fiore <andrea.fiore@composable.io>
2015-06-17 15:50:29 +0100

    Add asset hashing to default grunt compile task

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
         asset_hash: {
             options: {
                 preserveSourceMaps: true,
-                assetMap: isDev ? false : 'conf/assets.map',
+                assetMap: 'conf/assets.map',
                 hashLength: 8,
                 algorithm: 'md5',
                 srcBasePath: 'public/',
@@ -136,19 +136,10 @@ module.exports = function(grunt) {
             'sass',
             'requirejs',
             'copy:images',
-            'copy:jsVendor'
+            'copy:jsVendor',
+            'asset_hash',
+            'clean:assets'
         ]);
-
-        /**
-         * Only version files for prod builds
-         * Wipe out unused non-versioned assets for good measure
-         */
-        if (!isDev) {
-            grunt.task.run([
-                'asset_hash',
-                'clean:assets'
-            ]);
-        }
     });
 
     grunt.registerTask('default', ['compile']);
