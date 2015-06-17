f2f6ca4b5af294f2cc16c6a4192d6c889bd56915
Andrea Fiore <andrea.fiore@composable.io>
2015-06-17 14:13:06 +0100

    Fix assets path and refactor loading of assets.map

--- app/controllers/CachedAssets.scala
+++ app/controllers/CachedAssets.scala
@@ -1,26 +1,24 @@
 package controllers
 
 import play.api.Logger
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import play.api.libs.json.{JsObject, Json}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.io.Source
 
 object CachedAssets extends Controller {
-  private val logger = Logger(getClass)
-  private val map = {
-    val resourceOpt = Option(getClass.getClassLoader.getResourceAsStream("assets.map"))
-    val jsonOpt     = resourceOpt.map(Source.fromInputStream(_).mkString).map(Json.parse)
+  private val logger      = Logger(getClass)
+  private val hashedPaths: Map[String, String] =
+    (for {
+      resourceIs <- Option(getClass.getClassLoader.getResourceAsStream("assets.map"))
+    } yield Json.parse(resourceIs).as[Map[String, String]]).getOrElse {
 
-    jsonOpt.map(_.as[JsObject].fields.toMap.mapValues(_.as[String])).getOrElse {
       logger.warn("Could not find a config/assets.map file. You can generate it by running 'grunt compile'")
       Map.empty
     }
-  }
 
   def at(path: String, file: String, aggressiveCaching: Boolean = false) = Action.async { request =>
-    val hashedFile = map.getOrElse(file, file)
+    val hashedFile = hashedPaths.getOrElse(file, file)
 
     controllers.Assets.at(path, hashedFile, aggressiveCaching).apply(request).recover {
       case e: RuntimeException => {
