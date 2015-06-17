b8c9bb712677ca0f62c662bd082addec6726ab25
Andrea Fiore <andrea.fiore@composable.io>
2015-06-17 12:34:27 +0100

    Resolve hashed assets within CachedAssets controller

--- app/controllers/CachedAssets.scala
+++ app/controllers/CachedAssets.scala
@@ -1,14 +1,28 @@
 package controllers
 
-import play.Logger
+import play.api.Logger
 import play.api.mvc.{Action, Controller}
+import play.api.libs.json.{JsObject, Json}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.Source
 
 object CachedAssets extends Controller {
+  private val logger = Logger(getClass)
+  private val map = {
+    val resourceOpt = Option(getClass.getClassLoader.getResourceAsStream("assets.map"))
+    val jsonOpt     = resourceOpt.map(Source.fromInputStream(_).mkString).map(Json.parse)
+
+    jsonOpt.map(_.as[JsObject].fields.toMap.mapValues(_.as[String])).getOrElse {
+      logger.warn("Could not find a config/assets.map file. You can generate it by running 'grunt compile'")
+      Map.empty
+    }
+  }
 
   def at(path: String, file: String, aggressiveCaching: Boolean = false) = Action.async { request =>
-    controllers.Assets.at(path, file, aggressiveCaching).apply(request).recover {
+    val hashedFile = map.getOrElse(file, file)
+
+    controllers.Assets.at(path, hashedFile, aggressiveCaching).apply(request).recover {
       case e: RuntimeException => {
         Logger.warn(s"Asset run time exception for path $path $file. Does this file exist?", e)
         Cached(NotFound)
