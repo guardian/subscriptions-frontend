c8c7d3abc3b56ab47d243ac0690dbccadf15e293
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-06-18 11:56:32 +0100

    Added idWebAppUrlSignin for AuthenticationService

--- app/services/AuthenticationService.scala
+++ app/services/AuthenticationService.scala
@@ -5,7 +5,7 @@ import com.gu.identity.play.AuthenticationService
 import configuration.Config
 
 object AuthenticationService extends AuthenticationService {
-  override def idWebAppSigninUrl(returnUrl: String): String = "todo"
+  override def idWebAppSigninUrl(returnUrl: String): String = Config.Identity.webAppSigninUrl(returnUrl)
 
   override val identityKeys: IdentityKeys = Config.Identity.keys
 }
