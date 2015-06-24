3878d9f11804af9dcac5798ea31d0919bd1032d5
rtudor <tudorraul@gmail.com>
2015-06-24 13:46:05 +0100

    move form selectors in their own module

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,5 +1,5 @@
 require([
-    'modules/checkout',
+    'modules/checkout/checkout',
     'modules/digitalpack'
 ], function(
     checkout,
---
+++ assets/javascripts/modules/checkout/selectors.js
@@ -0,0 +1,20 @@
+define(['$'], function ($) {
+
+    return {
+        $FIRST_NAME: $('.js-checkout-first '),
+        $LAST_NAME: $('.js-checkout-last'),
+        $EMAIL: $('.js-checkout-email'),
+        $ADDRESS1: $('.js-checkout-house'),
+        $ADDRESS2: $('.js-checkout-street'),
+        $ADDRESS3: $('.js-checkout-town'),
+        $POSTCODE: $('.js-checkout-postcode'),
+        $ACCOUNT: $('.js-checkout-account'),
+        $SORTCODE1: $('.js-checkout-sortcode1'),
+        $SORTCODE2: $('.js-checkout-sortcode2'),
+        $SORTCODE3: $('.js-checkout-sortcode3'),
+        $HOLDER: $('.js-checkout-holder'),
+        $FIND_ADDRESS: $('.js-checkout-find-address'),
+        $MANUAL_ADDRESS: $('.js-checkout-manual-address'),
+        $FULL_ADDRESS: $('.js-checkout-full-address')
+    }
+});
