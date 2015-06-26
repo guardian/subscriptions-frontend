f77190e0d20bb0fda94b449181e417e436aee493
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-06-26 16:48:06 +0100

    Revert "Revert "Configuration for Touchpoint services - Salesforce and Zuora.""

---
+++ app/monitoring/Metrics.scala
@@ -0,0 +1,11 @@
+package monitoring
+
+import com.amazonaws.regions.{Region, Regions}
+import com.gu.monitoring.CloudWatch
+import configuration.Config
+
+trait Metrics extends CloudWatch {
+  val region = Region.getRegion(Regions.EU_WEST_1)
+  val stage = Config.stage
+  val application = Config.appName
+}
