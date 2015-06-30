6654376345b20112ddd401a60863cc6e39436e65
Andrea Fiore <andrea.fiore@composable.io>
2015-06-30 16:03:26 +0100

    Enable CSRF protection

--- app/Global.scala
+++ app/Global.scala
@@ -1,10 +1,11 @@
 import filters.CheckCacheHeadersFilter
+import play.filters.csrf.CSRFFilter
 import monitoring.SentryLogging
 import play.api.Application
 import play.api.mvc.WithFilters
 import services.TouchpointBackend
 
-object Global extends WithFilters(CheckCacheHeadersFilter) {
+object Global extends WithFilters(CheckCacheHeadersFilter, CSRFFilter()) {
   override def onStart(app: Application) {
     SentryLogging.init()
     TouchpointBackend.All.foreach(_.start())
