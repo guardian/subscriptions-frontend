2eab7225254901a34437d2febeeb45a9fc97c5e5
Andrea Fiore <andrea.fiore@composable.io>
2015-07-08 16:02:15 +0100

    Cover all collection and delivery subscriptions

--- test/functional/PrintSubscriptionsSpec.scala
+++ test/functional/PrintSubscriptionsSpec.scala
@@ -2,35 +2,41 @@ package functional
 
 import java.net.URL
 
-import controllers.routes.Shipping.{viewCollectionPaper, viewCollectionPaperDigital, viewDeliveryPaperDigital, viewDeliveryPaper}
 import configuration.Config.appUrl
 import functional.pages.{Home, SubscriptionPlan}
 import org.openqa.selenium.WebDriver
 import org.scalatest._
 import org.scalatest.selenium.WebBrowser
 
-case class Subscription(name: String, landingHost: String)
+case class SubscriptionTest(url: String, name: String, landingHost: String)
+
+object SubscriptionTest {
+  def collection(url: String, name: String) =
+    SubscriptionTest(url, name, "www.guardiansubscriptions.co.uk")
+  def delivery(url: String, name: String) =
+    SubscriptionTest(url, name, "www.guardiandirectsubs.co.uk")
+}
 
 class PrintSubscriptionsSpec extends FreeSpec with ShouldMatchers with WebBrowser with BeforeAndAfterAll {
   implicit lazy val driver: WebDriver = Driver.fromConfig
 
-  "selecting a subscription managed through " - {
+  val testData = Seq(
+    SubscriptionTest.collection(viewCollectionPaper.toString, "Paper voucher subscription"),
+    SubscriptionTest.collection(viewCollectionPaperDigital.toString, "Paper + digital voucher subscription"),
+    SubscriptionTest.delivery(viewDeliveryPaper.toString, "Paper home delivery subscription"),
+    SubscriptionTest.delivery(viewDeliveryPaperDigital.toString, "Paper + digital home delivery subscription")
+  )
+
+  "Lucrative subscriptions" - {
+    for (test <- testData) yield test.name.taggedAs(Acceptance) in {
+      goTo(s"$appUrl/${test.url}")
+      SubscriptionPlan.selectSixdayPackage()
+      assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
+      assertResult(test.landingHost)(currentHost)
+    }
+
     "Paper + Digital subscription" taggedAs Acceptance in {
       Home.selectPaperPlusDigital()
-      SubscriptionPlan.selectSixdayPackage()
-      assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
-      assertResult("www.guardiansubscriptions.co.uk")(currentHost)
-    }
-
-    "Paper voucher subscription" taggedAs Acceptance in {
-      goTo(s"$appUrl/${viewCollectionPaper.toString}")
-      SubscriptionPlan.selectSixdayPackage()
-      assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
-      assertResult("www.guardiansubscriptions.co.uk")(currentHost)
-    }
-
-    "Paper voucher + digital subscription" taggedAs Acceptance in {
-      goTo(s"$appUrl/${viewCollectionPaperDigital.toString}")
       SubscriptionPlan.selectSixdayPackage()
       assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
       assertResult("www.guardiansubscriptions.co.uk")(currentHost)
@@ -41,20 +47,6 @@ class PrintSubscriptionsSpec extends FreeSpec with ShouldMatchers with WebBrowse
       SubscriptionPlan.selectWeekendPackage()
       assert(pageHasText("You have chosen\nWEEKEND"), "document contains selected plan")
       assertResult("www.guardiansubscriptions.co.uk")(currentHost)
-    }
-
-    "Paper home delivery + digital subscription" taggedAs Acceptance in {
-      goTo(s"$appUrl/${viewDeliveryPaperDigital.toString}")
-      SubscriptionPlan.selectWeekendPackage()
-      assert(pageHasText("You have chosen\nWEEKEND"), "document contains selected plan")
-      assertResult("www.guardiandirectsubs.co.uk")(currentHost)
-    }
-
-    "Paper home delivery subscription" taggedAs Acceptance in {
-      goTo(s"$appUrl/${viewDeliveryPaper.toString}")
-      SubscriptionPlan.selectSixdayPackage()
-      assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
-      assertResult("www.guardiandirectsubs.co.uk")(currentHost)
     }
   }
 
