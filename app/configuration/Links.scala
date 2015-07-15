a98b3caee24086f9da7c93d4c7e32908217de379
David Rapson <david.rapson@guardian.co.uk>
2015-07-15 12:35:31 +0100

    Review details styling, add terms links

--- app/configuration/Links.scala
+++ app/configuration/Links.scala
@@ -3,6 +3,14 @@ package configuration
 case class Links(href: String, title: String)
 
 object Links {
+  val termsOfService = Links(
+    "http://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions",
+    "Terms of Service"
+  )
+  val privacyPolicy = Links(
+    "http://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions",
+    "Privacy Policy"
+  )
   val guardianSubscriptionFaqs = Links(
     "http://www.theguardian.com/subscriber-direct/subscription-frequently-asked-questions",
     "Frequently Asked Questions"
