5400d0f24fc9bc6a65c5768651561fed728e7e24
David Rapson <david.rapson@guardian.co.uk>
2015-07-17 11:28:49 +0100

    Improve modelling of Subscriptions
    - Model steps
    - Change prices to floats
    - Simplify view

--- app/controllers/Shipping.scala
+++ app/controllers/Shipping.scala
@@ -6,31 +6,30 @@ import play.api.mvc._
 
 object Shipping extends Controller {
 
-  def index(subscriptionCollection: SubscriptionCollection) = {
+  def index(subscriptionCollection: SubscriptionProduct) = {
     Ok(views.html.shipping.shipping(subscriptionCollection))
   }
 
   def viewCollectionPaperDigital() = CachedAction {
-    index(SubscriptionCollection(
+    index(CollectionSubscriptionProduct(
       title = "Paper + digital voucher subscription",
       description = "Save money on your newspapers and digital content. Plus start using the daily edition and premium live news app immediately.",
-      method = "collection",
       packageType = "paper-digital",
       options = Seq(
         SubscriptionOption("everyday",
-          "Everyday+", "£10.99", Some("54%"), "£47.62", "Guardian and Observer papers, plus tablet editions and Premium mobile access",
+          "Everyday+", 10.99f, Some("54%"), 47.62f, "Guardian and Observer papers, plus tablet editions and Premium mobile access",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv7&skip=1"
         ),
         SubscriptionOption("sixday",
-          "Sixday+", "£9.99", Some("53%"), "£43.29", "Guardian papers, plus tablet editions and Premium mobile access",
+          "Sixday+", 9.99f, Some("53%"), 43.29f, "Guardian papers, plus tablet editions and Premium mobile access",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv6&skip=1"
         ),
         SubscriptionOption("weekend",
-          "Weekend+", "£5.99", Some("60%"), "£25.96", "Saturday Guardian and Observer papers, plus tablet editions and Premium mobile access",
+          "Weekend+", 5.99f, Some("60%"), 25.96f, "Saturday Guardian and Observer papers, plus tablet editions and Premium mobile access",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv2&skip=1"
         ),
         SubscriptionOption("sunday",
-          "Sunday+", "£4.99", Some("60%"), "£21.62", "Observer paper, plus tablet editions and Premium mobile access",
+          "Sunday+", 4.99f, Some("60%"), 21.62f, "Observer paper, plus tablet editions and Premium mobile access",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=ov1&skip=1"
         )
       )
@@ -38,22 +37,21 @@ object Shipping extends Controller {
   }
 
   def viewCollectionPaper() = CachedAction {
-    index(SubscriptionCollection(
+    index(CollectionSubscriptionProduct(
       title = "Paper voucher subscription",
       description = "Save money on your newspapers.",
-      method = "collection",
       packageType = "paper",
       options = Seq(
         SubscriptionOption("everyday",
-          "Everyday", "£9.99", Some("32%"), "£43.29", "Guardian and Observer papers",
+          "Everyday", 9.99f, Some("32%"), 43.29f, "Guardian and Observer papers",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv7&skip=1"
         ),
         SubscriptionOption("sixday",
-          "Sixday", "£8.49", Some("27%"), "£36.79", "Guardian papers",
+          "Sixday", 8.49f, Some("27%"), 36.79f, "Guardian papers",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv6&skip=1"
         ),
         SubscriptionOption("weekend",
-          "Weekend", "£4.49", Some("20%"), "£19.46", "Saturday Guardian and Observer papers",
+          "Weekend", 4.49f, Some("20%"), 19.46f, "Saturday Guardian and Observer papers",
           "https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv2&skip=1"
         )
       )
@@ -61,27 +59,26 @@ object Shipping extends Controller {
   }
 
   def viewDeliveryPaperDigital() = CachedAction {
-    index(SubscriptionCollection(
+    index(DeliverySubscriptionProduct(
       title = "Paper + digital home delivery subscription",
       description = """|If you live within the M25 you can have your papers delivered by 7am Monday - Saturday and 8.30am on Sunday.
         |Plus you can start using the daily edition and premium live news app immediately.""".stripMargin,
-      method = "delivery",
       packageType = "paper-digital",
       options = Seq(
         SubscriptionOption("everyday",
-          "Everyday+", "£14.99", None, "£64.96", "Guardian and Observer papers, plus tablet editions and Premium mobile access",
+          "Everyday+", 14.99f, None, 64.96f, "Guardian and Observer papers, plus tablet editions and Premium mobile access",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=EVERYDAY%2B"
         ),
         SubscriptionOption("sixday",
-          "Sixday+", "£12.99", None, "£56.29", "Guardian papers, plus tablet editions and Premium mobile access",
+          "Sixday+", 12.99f, None, 56.29f, "Guardian papers, plus tablet editions and Premium mobile access",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SIXDAY%2B"
         ),
         SubscriptionOption("weekend",
-          "Weekend+", "£7.99", None, "£34.62", "Saturday Guardian and Observer papers, plus tablet editions and Premium mobile access",
+          "Weekend+", 7.99f, None, 34.62f, "Saturday Guardian and Observer papers, plus tablet editions and Premium mobile access",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=WEEKEND%2B"
         ),
         SubscriptionOption("sunday",
-          "Sunday+", "£5.99", None, "£25.96", "Observer paper, plus tablet editions and Premium mobile access",
+          "Sunday+", 5.99f, None, 25.96f, "Observer paper, plus tablet editions and Premium mobile access",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SUNDAY%2B"
         )
       )
@@ -89,22 +86,21 @@ object Shipping extends Controller {
   }
 
   def viewDeliveryPaper() = CachedAction {
-    index(SubscriptionCollection(
+    index(DeliverySubscriptionProduct(
       title = "Paper home delivery subscription",
       description = "If you live within the M25 you can have your papers delivered by 7am Monday - Saturday and 8.30 on Sunday.",
-      method = "delivery",
       packageType = "paper",
       options = Seq(
         SubscriptionOption("everyday",
-          "Everyday", "£13.99", None, "£60.62", "Guardian and Observer papers",
+          "Everyday", 13.99f, None, 60.62f, "Guardian and Observer papers",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=EVERYDAY"
         ),
         SubscriptionOption("sixday",
-          "Sixday", "11.49", None, "£49.79", "Guardian papers",
+          "Sixday", 11.49f, None, 49.79f, "Guardian papers",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SIXDAY"
         ),
         SubscriptionOption("weekend",
-          "Weekend", "£6.49", None, "£28.12", "Saturday Guardian and Observer papers",
+          "Weekend", 6.49f, None, 28.12f, "Saturday Guardian and Observer papers",
           "https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=WEEKEND"
         )
       )
--- app/model/Subscriptions.scala
+++ app/model/Subscriptions.scala
@@ -5,23 +5,52 @@ object Subscriptions {
   case class SubscriptionOption(
     id: String,
     title: String,
-    weeklyPrice: String,
+    weeklyPrice: Float,
     weeklySaving: Option[String],
-    monthlyPrice: String,
+    monthlyPrice: Float,
     description: String,
     url: String
   )
 
-  case class SubscriptionCollection(
+  trait SubscriptionProduct {
+    val title: String
+    val description: String
+    val packageType: String
+    val options: Seq[SubscriptionOption]
+    val stepsHeading: String
+    val steps: Seq[String]
+    val insideM25: Boolean
+    def capitalizedTitle = title.split("\\s").map(_.capitalize).mkString(" ")
+  }
+
+  case class DeliverySubscriptionProduct(
     title: String,
     description: String,
     packageType: String,
-    method: String,
     options: Seq[SubscriptionOption]
-  ) {
-    val isCollection = method == "collection"
-    val isPaperDigital = packageType == "paper-digital"
-    val capitalizedTitle = title.split("\\s").map(_.capitalize).mkString(" ")
+  ) extends SubscriptionProduct {
+    override val insideM25 = true
+    val stepsHeading = "This is how direct delivery works"
+    val steps = Seq(
+      "Pick the perfect package for you",
+      "Confirm your address is within the M25",
+      "Get your papers delivered to your door by 7am Monday - Saturday and 8:30am on Sundays"
+    )
+  }
+
+  case class CollectionSubscriptionProduct(
+    title: String,
+    description: String,
+    packageType: String,
+    options: Seq[SubscriptionOption]
+  ) extends SubscriptionProduct {
+    override val insideM25 = false
+    val stepsHeading = "This is how the voucher scheme works"
+    val steps = Seq(
+      "Pick the perfect package for you",
+      "We'll post personalised vouchers for the newspapers in your package",
+      "Redeem your vouchers at over 1,000 retailers across the UK"
+    )
   }
 
 }
