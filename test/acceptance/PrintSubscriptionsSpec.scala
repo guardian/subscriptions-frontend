967398d25d35b4522b15e2416ee0e2974c7c4d7c
Andrea Fiore <andrea.fiore@composable.io>
2015-07-17 12:04:55 +0100

    Add test runner script
    Also change spec style to Feature in PrintSubscriptionsSpec

--- test/acceptance/PrintSubscriptionsSpec.scala
+++ test/acceptance/PrintSubscriptionsSpec.scala
@@ -15,7 +15,7 @@ object SubscriptionTest {
     SubscriptionTest(url, name, "www.guardiandirectsubs.co.uk")
 }
 
-class PrintSubscriptionsSpec extends FreeSpec with Util with WebBrowser {
+class PrintSubscriptionsSpec extends FeatureSpec with Util with WebBrowser with GivenWhenThen {
   implicit lazy val driver: WebDriver = Config.driver
 
   val testData = Seq(
@@ -25,22 +25,22 @@ class PrintSubscriptionsSpec extends FreeSpec with Util with WebBrowser {
     SubscriptionTest.delivery("/delivery/paper-digital", "Paper + digital home delivery subscription")
   )
 
-  "Lucrative subscriptions" - {
-    for (test <- testData) yield test.name.taggedAs(Acceptance) in {
+  feature("Lucrative subscriptions") {
+    for (test <- testData) yield scenario(test.name, Acceptance) {
       goTo(s"$appUrl/${test.url}")
       SubscriptionPlan.selectSixdayPackage()
       assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
       assertResult(test.landingHost)(currentHost)
     }
 
-    "Paper + Digital subscription" taggedAs Acceptance in {
+    scenario("Paper + Digital subscription", Acceptance) {
       Home.selectPaperPlusDigital()
       SubscriptionPlan.selectSixdayPackage()
       assert(pageHasText("You have chosen\nSIXDAY"), "document contains selected plan")
       assertResult("www.guardiansubscriptions.co.uk")(currentHost)
     }
 
-    "Paper only subscription" taggedAs Acceptance in {
+    scenario("Paper only subscription", Acceptance) {
       Home.selectPaper()
       SubscriptionPlan.selectWeekendPackage()
       assert(pageHasText("You have chosen\nWEEKEND"), "document contains selected plan")
@@ -48,14 +48,14 @@ class PrintSubscriptionsSpec extends FreeSpec with Util with WebBrowser {
     }
   }
 
-  "Digital Pack" - {
-    "selecting UK" taggedAs Acceptance in {
+  feature("Digital Pack") {
+    scenario("selecting UK", Acceptance){
       pending
       DigitalPack.selectUK
       assert(pageHasText("You have chosen:\nDigital pack"))
     }
 
-    "selecting not UK" taggedAs Acceptance in {
+    scenario("selecting not UK", Acceptance) {
       pending
       DigitalPack.selectNonUK
       assertResult("www.guardiansubscriptions.co.uk")(currentHost)
