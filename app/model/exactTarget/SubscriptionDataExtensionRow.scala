91699a34755e34f519b5f850dd92da2a35119d82
Thomas Franquelin <ostapneko@gmail.com>
2015-07-28 17:25:00 +0100

    Format date for ExactTarget data extension rows

--- app/model/exactTarget/SubscriptionDataExtensionRow.scala
+++ app/model/exactTarget/SubscriptionDataExtensionRow.scala
@@ -2,6 +2,7 @@ package model.exactTarget
 
 import com.gu.membership.zuora.soap.Zuora._
 import model.SubscriptionData
+import org.joda.time.DateTime
 
 object SubscriptionDataExtensionRow {
   def apply(
@@ -15,6 +16,9 @@ object SubscriptionDataExtensionRow {
     val billingPeriod = ratePlanCharge.billingPeriod.getOrElse(
       throw new ExactTargetException(s"Error while processing $subscription: Could not create a SubscriptionDataExtensionRow without a billing period")
     )
+
+    // FIXME bogus payment day while we figure out when to get it properly
+    val secondPaymentDate = formatDate(DateTime.now().plusYears(100))
 
     val personalData = subscriptionData.personalData
 
@@ -39,14 +43,29 @@ object SubscriptionDataExtensionRow {
       `Account Name` -> paymentData.holder,
       `Sort Code` -> paymentData.sortCode,
       `Account number` -> paymentData.account,
-      `Date of first payment` -> subscription.termStartDate.toString,
-      `Date of second payment` -> ratePlanCharge.chargedThroughDate.toString,
+      `Date of first payment` -> formatDate(subscription.termStartDate),
+      `Date of second payment` -> secondPaymentDate,
       `Currency` -> account.currency,
       //TODO to remove, hardcoded in the template
       `Trial period` -> "14",
       `MandateID` -> paymentMethod.mandateId,
       `Email` -> personalData.email
     )
+  }
+
+  private def formatDate(dateTime: DateTime) = {
+    val day = dateTime.dayOfMonth.getAsString
+    val daySuffix = day.last match {
+      case '1' => "st"
+      case '2' => "nd"
+      case '3' => "rd"
+      case _   => "th"
+    }
+    val month = dateTime.monthOfYear.getAsText
+
+    val year = dateTime.year.getAsString
+
+    s"$day$daySuffix $month $year"
   }
 }
 
