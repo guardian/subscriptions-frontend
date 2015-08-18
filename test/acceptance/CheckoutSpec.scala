89d817e030f7c329fafb0792cbcd39b99ea88e05
tudorraul <tudorraul@gmail.com>
2015-08-18 11:52:44 +0100

    fix checkout form acceptance tests

--- test/acceptance/CheckoutSpec.scala
+++ test/acceptance/CheckoutSpec.scala
@@ -15,48 +15,44 @@ class CheckoutSpec extends FeatureSpec with WebBrowser with Util with GivenWhenT
 
   feature("Checkout page") {
     scenario("Guest user completes a checkout and sets an account password", Acceptance) {
-      withQACookie {
-        val checkout = new pages.Checkout
-        When("I visit the checkout page ")
-        go.to(checkout)
+      val checkout = new pages.Checkout
+      When("I visit the checkout page ")
+      go.to(checkout)
 
-        And("I fill in personal details")
-        checkout.fillInPersonalDetails()
+      And("I fill in personal details")
+      checkout.fillInPersonalDetails()
 
-        And("I fill in payment details")
-        checkout.fillInPaymentDetails()
+      And("I fill in payment details")
+      checkout.fillInPaymentDetails()
 
-        And("I submit the form")
-        checkout.submit()
+      And("I submit the form")
+      checkout.submit()
 
-        Then("I should land on the Thank You page")
-        assert(pageHasText("Thank you for your order"))
+      Then("I should land on the Thank You page")
+      assert(pageHasText("Thank you for your order"))
 
-        When("I set a password for my account")
-        new ThankYou().setPassword("supers4af3passw0rd")
+      When("I set a password for my account")
+      new ThankYou().setPassword("supers4af3passw0rd")
 
-        Then("I should read a confirmation message")
-        assert(pageHasText("All done"))
-      }
+      Then("I should read a confirmation message")
+      assert(pageHasText("All done"))
     }
 
     scenario("Guest user supplies invalid details", Acceptance) {
-      withQACookie {
-        val checkout = new Checkout()
-        import checkout.PersonalDetails
+      val checkout = new Checkout()
+      import checkout.PersonalDetails
 
-        When("I visit the checkout page")
-        go.to(checkout)
+      When("I visit the checkout page")
+      go.to(checkout)
 
-        And("I fill in personal details with inconsistent emails")
-        PersonalDetails.fillIn()
-        PersonalDetails.emailConfirmation.value = "non-matching-email@example.com"
+      And("I fill in personal details with inconsistent emails")
+      PersonalDetails.fillIn()
+      PersonalDetails.emailConfirmation.value = "non-matching-email@example.com"
 
-        PersonalDetails.continue()
+      PersonalDetails.continue()
 
-        Then("The email field should be marked with an error")
-        assert(PersonalDetails.emailNotValid(), "email confirmation should not be valid")
-      }
+      Then("The email field should be marked with an error")
+      assert(PersonalDetails.emailNotValid(), "email confirmation should not be valid")
     }
   }
 }
