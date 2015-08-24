0bcca224568d4c1709ada519684fa29c9efcc3b8
Thomas Franquelin <ostapneko@gmail.com>
2015-08-24 11:45:29 +0100

    Update final review copy

--- app/configuration/Links.scala
+++ app/configuration/Links.scala
@@ -8,9 +8,15 @@ object Links {
     "Terms and Conditions"
   )
   val privacyPolicy = Links(
-    "http://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions",
+    "http://www.theguardian.com/help/privacy-policy",
     "Privacy Policy"
   )
+
+  val whyYourDataMattersToUs = Links(
+    "https://www.theguardian.com/info/video/2014/nov/03/why-your-data-matters-to-us-video",
+    "Why your data matter to us"
+  )
+
   val guardianSubscriptionFaqs = Links(
     "http://www.theguardian.com/subscriber-direct/subscription-frequently-asked-questions",
     "Frequently Asked Questions"
