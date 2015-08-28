349114c4d4c69628bac4f3b691df21e269268e04
Chris Owen <chris.j.owen@guardian.co.uk>
2015-08-28 17:03:49 +0100

    Added sales force auth check to the healthcheck service

--- app/controllers/Healthcheck.scala
+++ app/controllers/Healthcheck.scala
@@ -9,8 +9,8 @@ object Healthcheck extends Controller with LazyLogging{
 
   def index = Action.async {
       (for {
-       //TODO: Touch salesforce
         products <- TouchpointBackend.Normal.zuoraService.products if products.nonEmpty
+        sfAuth <- TouchpointBackend.Normal.salesforceService.repo.salesforce.getAuthentication
       } yield Ok("OK"))
       .recover { case t: Throwable =>
         logger.error("Health check failed", t)
