4427d4aba8aee5da5fcde6b2b3f2bd92246f7bb1
Chris Owen <chris.j.owen@guardian.co.uk>
2015-09-01 16:06:12 +0100

    Changed cookie name to uppercase to fit with analitics cookie dropped, and added a percentage property to switch so we can increase over time

--- assets/javascripts/modules/country.js
+++ assets/javascripts/modules/country.js
@@ -4,16 +4,15 @@ define([
 ], function (elements, cookie) {
     'use strict';
 
-    var COUNTRY_FLOW_COOKIE_NAME = 'country-flow';
+    var COUNTRY_FLOW_COOKIE_NAME = 'COUNTRY_FLOW';
 
-    function shouldSwitch() {
-        return Math.floor(Math.random() * 2) === 1;
+    function shouldSwitch(percent) {
+        return Math.floor(Math.random() * 101) <= percent;
     }
 
-    function getCountryFlowCookieInfo() {
+    function loadCountryInfo() {
         var countryFlowCookie = cookie.getCookie(COUNTRY_FLOW_COOKIE_NAME);
         return (countryFlowCookie) ? JSON.parse(countryFlowCookie) : null;
-
     }
 
     function switchUrl() {
@@ -21,9 +20,9 @@ define([
     }
 
     function init() {
-        var cookieInfo = getCountryFlowCookieInfo();
+        var cookieInfo = loadCountryInfo();
         if (!cookieInfo) {
-            cookieInfo = {switchUrl: shouldSwitch()};
+            cookieInfo = {switchUrl: shouldSwitch(50)};
             cookie.setCookie(COUNTRY_FLOW_COOKIE_NAME, JSON.stringify(cookieInfo));
         }
         if (cookieInfo.switchUrl) {
