0b6b7c7c79334af91869ff4852ab4759fc9cd716
Chris J Owen <chris.j.owen@guardian.co.uk>
2015-10-05 14:23:41 +0100

    Formatting fix

--- app/controllers/Checkout.scala
+++ app/controllers/Checkout.scala
@@ -128,14 +128,12 @@ object Checkout extends Controller with LazyLogging with ActivityTracking {
         for {
           userId <- session.get(SessionKeys.UserId)
 	  token <- session.get(SessionKeys.IdentityGuestPasswordSettingToken)
-          form <- GuestUser(UserId(userId), IdentityToken(token)).toGuestAccountForm
-        } yield form
-      } {
-	const(None)
-      } // Don't display the user registration form if the user is logged in
+	  form <- GuestUser(UserId(userId), IdentityToken(token)).toGuestAccountForm
+	} yield form
+      }{ const(None) } // Don't display the user registration form if the user is logged in
 
       zuoraService.products.map { products =>
-        val product = products.find(_.ratePlanId == ratePlanId).getOrElse(
+	val product = products.find(_.ratePlanId == ratePlanId).getOrElse(
           throw new NoSuchElementException(s"Could not find a product with rate plan id $ratePlanId")
         )
         Ok(view.thankyou(subsName, passwordForm, touchpointBackend, product))
