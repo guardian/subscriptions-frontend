2c896f0a7bab8cafad67af6c2eb15b69fc8df6a6
tverran <tom.verran@guardian.co.uk>
2015-10-07 17:49:21 +0100

    use a less ambiguous parameter name

--- app/model/DigitalEdition.scala
+++ app/model/DigitalEdition.scala
@@ -9,8 +9,8 @@ object DigitalEdition {
   object US extends DigitalEdition("us", "US", "$19.99", "dis_2378")
   object AU extends DigitalEdition("au", "Australia", "$21.50", "dis_2379")
 
-  def getRedirect(e: DigitalEdition): String = {
-    e match {
+  def getRedirect(edition: DigitalEdition): String = {
+    edition match {
       case UK => "/digital/country"
       case DigitalEdition(id, name, price, cmp) => "https://www.guardiansubscriptions.co.uk/digitalsubscriptions/?prom=DGA38&CMP=" + cmp
     }
