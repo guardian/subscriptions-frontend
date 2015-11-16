9b8b4aa07b7e4490b3bd54666c7fc7fddca2960d
Joseph Smith <joseph.smith@guardian.co.uk>
2015-11-16 16:07:16 +0000

    Simplify JSON serialisation

--- app/model/JsVars.scala
+++ app/model/JsVars.scala
@@ -3,33 +3,18 @@ package model
 import play.api.libs.json._
 
 object JsVars {
-  def default = JsVars(
-    userIsSignedIn = false,
-    ignorePageLoadTracking = false,
-    stripePublicKey = None
-  )
+  def default = JsVars()
 
   implicit val jsVarsWrites = new Writes[JsVars] {
-    def writes(jsVars: JsVars) = {
-      val mandatory = Json.obj(
-        "user" -> Json.obj(
-          "isSignedIn" -> jsVars.userIsSignedIn,
-          "ignorePageLoadTracking" -> jsVars.ignorePageLoadTracking
-        )
-      )
-
-      val optional = jsVars.stripePublicKey match {
-        case Some(stripePublicKey) => Json.obj("stripePublicKey" -> stripePublicKey)
-        case None => Json.obj()
-      }
-
-      mandatory ++ optional
-    }
+    private val innerWrites = Json.writes[JsVars]
+    def writes(jsVars: JsVars) = Json.obj(
+      "user" -> Json.toJson(jsVars)(innerWrites)
+    )
   }
 }
 
 case class JsVars(
-  userIsSignedIn: Boolean,
-  ignorePageLoadTracking: Boolean,
-  stripePublicKey: Option[String]
+  userIsSignedIn: Boolean = false,
+  ignorePageLoadTracking: Boolean = false,
+  stripePublicKey: Option[String] = None
 )
