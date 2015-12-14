6937278677065d4ef83376775611a9da8c8e4619
tverran <tom.verran@guardian.co.uk>
2015-12-14 14:18:50 +0000

    Use a URI library

--- app/model/DigitalEdition.scala
+++ app/model/DigitalEdition.scala
@@ -1,5 +1,8 @@
 package model
 
+import com.netaporter.uri.Uri
+import com.netaporter.uri.dsl._
+
 case class DigitalEdition(id: String, name: String, price: String, campaign: String)
 
 
@@ -9,13 +12,17 @@ object DigitalEdition {
   object US extends DigitalEdition("us", "US", "$19.99", "dis_2378")
   object AU extends DigitalEdition("au", "Australia", "$21.50", "dis_2379")
 
-  def getRedirect(edition: DigitalEdition): String = edition match {
+  def getRedirect(edition: DigitalEdition): Uri = edition match {
       case UK => "/digital/country"
       case _ => "https://www.guardiansubscriptions.co.uk/digitalsubscriptions/?prom=dga38&CMP=FAB_3062"
   }
 
-  def getMembershipLandingPage(edition: DigitalEdition): String =  edition match {
-    case US => "https://membership.theguardian.com/us/supporter"
-    case _ => "https://membership.theguardian.com/join"
+  def getMembershipLandingPage(edition: DigitalEdition): Uri = {
+    val params = "INTCMP" -> s"GU_SUBSCRIPTIONS_${edition.id.toUpperCase}_PROMO"
+
+    edition match {
+      case US => "https://membership.theguardian.com/us/supporter" ? params
+      case _ => "https://membership.theguardian.com/join" ? params
+    }
   }
 }
