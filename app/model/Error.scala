c577656d60084f3eaad0c54f105bb398c1536e1e
Mario Galic <mario.galic@guardian.co.uk>
2016-03-15 15:21:09 +0000

    Prettify Sentry error messages

--- app/model/Error.scala
+++ app/model/Error.scala
@@ -6,12 +6,17 @@ trait SubsError {
   val response: Option[String]
 
   def toStringPretty(): String = {
-    val line1  = s"\n----------------------------------------"
-    val error  = s"\nError: ${this.getClass.getSimpleName}\n"
-    val line2   = s"----------------------------------------\n"
-    val strMsg = s"\n\tMessage:  \n\t\t ${message}"
-    val strIn  = s"\n\tInput:    \n\t\t ${request}"
-    val strOut = s"\n\tOutput:   \n\t\t ${response.getOrElse("")}"
-    line1 + error + line2 + strMsg + strIn + strOut
+    val line1  = s"\n---------------------------------------"
+    val error  = s"\n${this.getClass.getSimpleName}\n"
+    val strMsg = s"\tMessage:  \t ${message}"
+    val strIn  = s"\n\tInput:    \t ${request}"
+    val strOut = s"\n\tOutput:   \t ${response.getOrElse("")}"
+    line1 + error + strMsg + strIn + strOut
+  }
+}
+
+object SubsError {
+  def toStringPretty(seqErr: Seq[SubsError]): String = {
+    seqErr.head.message + ":\n" + seqErr.map(_.toStringPretty()).toString()
   }
 }
