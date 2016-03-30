9103949dbf1f5a5236f99b8cb60326a2fbc6b6b4
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2016-03-30 16:16:24 +0100

    URI decode Identity display name

--- assets/javascripts/utils/user.js
+++ assets/javascripts/utils/user.js
@@ -10,7 +10,7 @@ define(['utils/cookie'], function(cookie){
     function idCookieAdapter(data) {
         return {
             id: data[0],
-            displayname: data[2],
+            displayname: decodeURIComponent(data[2]),
             accountCreatedDate: data[6],
             emailVerified: data[7]
         };
