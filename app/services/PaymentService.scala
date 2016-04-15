c94884bd0ca2b225daf16d5df381ee4fa7a8ccc9
Joseph Smith <joseph.smith@guardian.co.uk>
2016-04-15 16:07:32 +0100

    Create guest identity account only after successful salesforce call
    Don't pass userId back to the browser on checkout error
    
    Cleanup
    
    Bump membership-common version number
    
    Add yield because foreach is weird for EitherT

--- app/services/PaymentService.scala
+++ app/services/PaymentService.scala
@@ -1,11 +1,11 @@
 package services
 
-import com.gu.i18n.{Currency, GBP, Country}
-import com.gu.memsub.{Current, PaidPlan, BillingPeriod}
+import com.gu.i18n.{Country, Currency, GBP}
+import com.gu.memsub.BillingPeriod
 import com.gu.salesforce.ContactId
 import com.gu.stripe.StripeService
 import com.gu.subscriptions.DigipackPlan
-import com.gu.zuora.soap.models.Commands.{PaymentMethod, CreditCardReferenceTransaction, BankTransfer, Account}
+import com.gu.zuora.soap.models.Commands.{Account, BankTransfer, CreditCardReferenceTransaction, PaymentMethod}
 import model._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
@@ -33,11 +33,12 @@ trait PaymentService {
       ))
   }
 
-  class CreditCardPayment(val paymentData: CreditCardData, val currency: Currency, userIdData: UserIdData, val memberId: ContactId) extends Payment {
-    override def makeAccount = Account.stripe(memberId, currency, autopay = true)
-    override def makePaymentMethod =
-      stripeService.Customer.create(userIdData.id.id, paymentData.stripeToken)
+  class CreditCardPayment(val paymentData: CreditCardData, val currency: Currency, val purchaserIds: PurchaserIdentifiers) extends Payment {
+    override def makeAccount = Account.stripe(purchaserIds.memberId, currency, autopay = true)
+    override def makePaymentMethod = {
+      stripeService.Customer.create(description = purchaserIds.description, card = paymentData.stripeToken)
         .map(a => CreditCardReferenceTransaction(a.card.id, a.id))
+    }
   }
 
   def makeDirectDebitPayment(paymentData: DirectDebitData, personalData: PersonalData, memberId: ContactId) = {
@@ -46,14 +47,13 @@ trait PaymentService {
   }
 
   def makeCreditCardPayment(
-      paymentData: CreditCardData,
-      personalData: PersonalData,
-      userIdData: UserIdData,
-      memberId: ContactId,
-      plan: DigipackPlan[BillingPeriod]) = {
+     paymentData: CreditCardData,
+     personalData: PersonalData,
+     purchaserIds: PurchaserIdentifiers,
+     plan: DigipackPlan[BillingPeriod]) = {
     val desiredCurrency = personalData.currency
     val currency = if (plan.currencies.contains(desiredCurrency)) desiredCurrency else GBP
 
-    new CreditCardPayment(paymentData, currency, userIdData, memberId = memberId)
+    new CreditCardPayment(paymentData, currency, purchaserIds)
   }
 }
