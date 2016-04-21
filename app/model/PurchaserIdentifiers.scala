2500b9e684ee06bfb6b1e490a8a5af713d52ef7a
Mario Galic <mario.galic@guardian.co.uk>
2016-04-21 16:10:41 +0100

    Fix logging of purchaserId data during error

--- app/model/PurchaserIdentifiers.scala
+++ app/model/PurchaserIdentifiers.scala
@@ -4,5 +4,5 @@ import com.gu.identity.play.IdMinimalUser
 import com.gu.salesforce.ContactId
 
 case class PurchaserIdentifiers(memberId: ContactId, identityId: Option[IdMinimalUser]) {
-  val description = (Seq(s"SalesforceContactID - ${memberId.salesforceContactId}") ++ identityId.map(id => s"IdentityID - $id")).mkString("\n")
+  val description = (Seq(s"SalesforceContactID - ${memberId.salesforceContactId}") ++ identityId.map(id => s"IdentityID - $id")).mkString(" ")
 }
