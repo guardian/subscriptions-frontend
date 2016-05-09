928f2d55978510bcd7b9c5cd9c23ad55015dc4ee
Paul Brown <paul.brown@guardian.co.uk>
2016-05-09 14:56:52 +0100

    Tweaks to remove borders when not necessary. Plus code review tweaks.

--- app/views/support/LandingPageOps.scala
+++ app/views/support/LandingPageOps.scala
@@ -2,12 +2,13 @@ package views.support
 
 import com.gu.memsub.promo.Promotion.{AnyPromotion, AnyPromotionWithLandingPage}
 import com.gu.memsub.promo._
+import org.joda.time.DateTime.now
 
 object LandingPageOps {
 
   private def getSectionColour(promotion: AnyPromotionWithLandingPage) = promotion.landingPage.sectionColour.getOrElse(Blue)
 
-  implicit class ToLandingPageClassName(promotion: AnyPromotionWithLandingPage) {
+  implicit class ForPromotionsWithALandingPage(promotion: AnyPromotionWithLandingPage) {
     def landingPageSectionColour: String = {
       getSectionColour(promotion) match {
         case Blue => "section-blue"
@@ -15,18 +16,21 @@ object LandingPageOps {
         case White => "section-white"
       }
     }
-  }
-
-  implicit class GetSectionSeparator(promotion: AnyPromotionWithLandingPage) {
     def getSectionSeparator: String = {
       getSectionColour(promotion) match {
         case White => "section-separator"
         case _ => if (promotion.whenFreeTrial.isDefined) "section-separator" else ""
       }
     }
+    def getDescriptionBorder: String = {
+      if (promotion.landingPage.imageUrl.isDefined && promotion.expires.exists(now.plusMonths(1).isAfter))
+        "promotion-description--bordered"
+      else
+        ""
+    }
   }
 
-  implicit class GetIncentiveTermsAndConditions(promotion: AnyPromotion) {
+  implicit class ForAnyPromotions(promotion: AnyPromotion) {
     def getIncentiveTermsAndConditions: String = {
       promotion.whenIncentive.fold("") { p => p.promotionType.termsAndConditions.mkString }
     }
