d8b8cd3bd4a7e47dca37d91f27f6866307a0ef8b
Paul Brown <paul.brown@guardian.co.uk>
2016-05-23 11:06:00 +0100

    Remove the X-Frame-Options header on the promotion preview page.

---
+++ app/filters/AffectXFrameOptionsHeader.scala
@@ -0,0 +1,22 @@
+package filters
+
+import configuration.Config
+import controllers.Cached._
+import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.ws.WS
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+object AffectXFrameOptionsHeader extends Filter {
+  def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    nextFilter(requestHeader).map { result =>
+      if (requestHeader.path.startsWith("/q/")) {
+        result.withHeaders("X-Frame-Options" -> "")
+      } else {
+        result
+      }
+    }
+  }
+}
