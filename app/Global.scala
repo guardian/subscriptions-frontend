d289fa8a4a57d621d425b38d29e01f69683f71cf
tverran <tom.verran@guardian.co.uk>
2016-06-07 11:26:42 +0100

    Add support for previewing promotions via POST

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,7 @@
-import filters.{AddEC2InstanceHeader, AddGuIdentityHeaders, HandleXFrameOptionsOverrideHeader, CheckCacheHeadersFilter}
+import filters.{AddEC2InstanceHeader, AddGuIdentityHeaders, CheckCacheHeadersFilter, HandleXFrameOptionsOverrideHeader}
 import monitoring.SentryLogging
 import play.api.Application
-import play.api.mvc.WithFilters
+import play.api.mvc.{EssentialAction, EssentialFilter, WithFilters}
 import play.filters.csrf.CSRFFilter
 import play.filters.headers.{SecurityHeadersConfig, SecurityHeadersFilter}
 
@@ -13,10 +13,29 @@ object Global extends WithFilters(
     permittedCrossDomainPolicies = None,
     contentSecurityPolicy = None
   )),
-  CSRFFilter(),
+  new ExcludingCSRFFilter(CSRFFilter()),
   AddGuIdentityHeaders,
   AddEC2InstanceHeader) {
   override def onStart(app: Application) {
     SentryLogging.init()
+  }
+}
+
+// taken from http://dominikdorn.com/2014/07/playframework-2-3-global-csrf-protection-disable-csrf-selectively/
+// play 2.5 removes the need for this by considering trusted CORS routes exempt from CSRF
+private class ExcludingCSRFFilter(filter: CSRFFilter) extends EssentialFilter {
+
+  override def apply(nextFilter: EssentialAction) = new EssentialAction {
+
+    import play.api.mvc._
+
+    override def apply(rh: RequestHeader) = {
+      val chainedFilter = filter.apply(nextFilter)
+      if (rh.tags.getOrElse("ROUTE_COMMENTS", "").contains("NOCSRF")) {
+        nextFilter(rh)
+      } else {
+        chainedFilter(rh)
+      }
+    }
   }
 }
