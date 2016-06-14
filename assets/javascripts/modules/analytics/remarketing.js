0f2be8743dda9ac9be8c5fe010f3c065a7a35bba
tverran <tom.verran@guardian.co.uk>
2016-06-14 12:46:45 +0100

    Fix remarketing not using curl

--- assets/javascripts/modules/analytics/remarketing.js
+++ assets/javascripts/modules/analytics/remarketing.js
@@ -4,7 +4,7 @@ define(['modules/analytics/analyticsEnabled'], function(analyticsEnabled) {
     var remarketingUrl = 'https://www.googleadservices.com/pagead/conversion_async.js';
 
     function init() {
-        return require(['js!' + remarketingUrl], function () {
+        return curl(['js!' + remarketingUrl], function () {
             window.google_trackConversion({
                 google_conversion_id: 971225648,
                 google_custom_params: window.google_tag_params,
