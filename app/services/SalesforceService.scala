663c74115ec313145d5bcc390622b412937f4291
tverran <tom.verran@guardian.co.uk>
2016-07-05 09:49:15 +0100

    Test the serialisation of PersonalData to Salesforce

--- app/services/SalesforceService.scala
+++ app/services/SalesforceService.scala
@@ -12,29 +12,30 @@ import scala.concurrent.Future
 
 trait SalesforceService extends LazyLogging {
   def repo: SimpleContactRepository
-
   def createOrUpdateUser(personalData: PersonalData, deliveryAddress: Option[Address], userId: Option[IdMinimalUser]): Future[ContactId]
-
-  def createSalesforceUserData(personalData: PersonalData, deliveryAddress: Option[Address]): JsObject = Json.obj(
-      Keys.EMAIL -> personalData.email,
-      Keys.FIRST_NAME -> personalData.first,
-      Keys.LAST_NAME -> personalData.last,
-      Keys.BILLING_STREET -> personalData.address.line,
-      Keys.BILLING_CITY -> personalData.address.town,
-      Keys.BILLING_POSTCODE -> personalData.address.postCode,
-      Keys.BILLING_COUNTRY -> personalData.address.countryName,
-      Keys.ALLOW_GU_RELATED_MAIL -> personalData.receiveGnmMarketing
-  ) ++ deliveryAddress.fold(Json.obj())(addr => Json.obj(
-      Keys.MAILING_STREET -> addr.line,
-      Keys.MAILING_CITY -> addr.town,
-      Keys.MAILING_POSTCODE -> addr.postCode,
-      Keys.MAILING_COUNTRY -> addr.countryName
-  ))
 }
 
 class SalesforceServiceImp(val repo: SimpleContactRepository) extends SalesforceService {
   override def createOrUpdateUser(personalData: PersonalData, deliveryAddress: Option[Address], userId: Option[IdMinimalUser]): Future[ContactId] =
-    repo.upsert(userId.map(_.id), createSalesforceUserData(personalData, deliveryAddress))
+    repo.upsert(userId.map(_.id), SalesforceService.createSalesforceUserData(personalData, deliveryAddress))
+}
+
+object SalesforceService {
+  def createSalesforceUserData(personalData: PersonalData, deliveryAddress: Option[Address]): JsObject = Json.obj(
+    Keys.EMAIL -> personalData.email,
+    Keys.FIRST_NAME -> personalData.first,
+    Keys.LAST_NAME -> personalData.last,
+    Keys.BILLING_STREET -> personalData.address.line,
+    Keys.BILLING_CITY -> personalData.address.town,
+    Keys.BILLING_POSTCODE -> personalData.address.postCode,
+    Keys.BILLING_COUNTRY -> personalData.address.countryName,
+    Keys.ALLOW_GU_RELATED_MAIL -> personalData.receiveGnmMarketing
+  ) ++ deliveryAddress.fold(Json.obj())(addr => Json.obj(
+    Keys.MAILING_STREET -> addr.line,
+    Keys.MAILING_CITY -> addr.town,
+    Keys.MAILING_POSTCODE -> addr.postCode,
+    Keys.MAILING_COUNTRY -> addr.countryName
+  ))
 }
 
 case class SalesforceServiceError(s: String) extends Throwable {
---
+++ test/services/SalesforceServiceTest.scala
@@ -0,0 +1,40 @@
+package services
+import com.gu.i18n.Title
+import com.gu.memsub.Address
+import model.PersonalData
+import org.specs2.mutable.Specification
+import com.gu.salesforce.ContactDeserializer.Keys._
+
+import scalaz.syntax.std.option._
+import play.api.libs.json.{JsString, Json}
+
+class SalesforceServiceTest extends Specification {
+
+  "Salesforce contact serialiser" should {
+
+    val bloggsResidence = Address("Flat 123", "123 Fake Street", "Faketown", "Kent", "FT1 0HF", "UK")
+    val data = PersonalData("Joe", "Bloggs", "joebloggs@example,com", receiveGnmMarketing = true, bloggsResidence, Some("1234"), Title.Prof.some)
+
+    "Serialise your good old fashioned basic fields" in {
+      SalesforceService.createSalesforceUserData(personalData = data, None) mustEqual Json.obj(
+        EMAIL -> data.email,
+        FIRST_NAME -> data.first,
+        LAST_NAME -> data.last,
+        BILLING_STREET -> data.address.line,
+        BILLING_CITY -> data.address.town,
+        BILLING_POSTCODE -> data.address.postCode,
+        BILLING_COUNTRY -> data.address.countryName,
+        ALLOW_GU_RELATED_MAIL -> data.receiveGnmMarketing
+      )
+    }
+
+    "Include your mailing address if you supply one" in {
+      val delivery = Address("Flat 456", "123 Delivery Grove", "Deliverytown", "Surrey", "DL1 ABC", "UK")
+      val salesforceInfo = SalesforceService.createSalesforceUserData(data, delivery.some)
+      (salesforceInfo \ MAILING_STREET).get mustEqual JsString(delivery.line)
+      (salesforceInfo \ MAILING_CITY).get mustEqual JsString(delivery.town)
+      (salesforceInfo \ MAILING_POSTCODE).get mustEqual JsString(delivery.postCode)
+      (salesforceInfo \ MAILING_COUNTRY).get mustEqual JsString(delivery.countryName)
+    }
+  }
+}
