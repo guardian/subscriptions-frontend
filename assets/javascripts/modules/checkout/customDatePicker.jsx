00f8514d0c677c9b0053ce2ee5bfd947aa827aee
Paul Brown <paul.brown@guardian.co.uk>
2016-07-12 11:01:57 +0100

    Added a filter to the date picker based on the chosen package days!

--- assets/javascripts/modules/checkout/customDatePicker.jsx
+++ assets/javascripts/modules/checkout/customDatePicker.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import DatePicker from 'react-datepicker'
 import moment from 'moment'
 
-import formElements from './formElements'
-
-var NUMBER_OF_DAYS_IN_ADVANCE = 5;
-var FIRST_SELECTABLE_DATE = moment().add(NUMBER_OF_DAYS_IN_ADVANCE, 'days');
-
-var NUMBER_OF_DAYS_AVAILABLE = 21;
-var LAST_SELECTABLE_DATE = moment().add(NUMBER_OF_DAYS_IN_ADVANCE, 'days').add(NUMBER_OF_DAYS_AVAILABLE, 'days');
-
 export default React.createClass({
     displayName: 'CustomDatePick',
+
+    getDefaultProps () {
+        return {
+            minDate: moment()
+        }
+    },
+
     getInitialState () {
         return {
-            startDate: FIRST_SELECTABLE_DATE
+            startDate: this.props.minDate
         }
     },
 
@@ -26,11 +25,8 @@ export default React.createClass({
 
     render () {
         return <DatePicker
-                    locale="en"
                     selected={this.state.startDate}
                     onChange={this.handleChange}
-                    minDate={FIRST_SELECTABLE_DATE}
-                    maxDate={LAST_SELECTABLE_DATE}
                     {...this.props} />
     }
 })
