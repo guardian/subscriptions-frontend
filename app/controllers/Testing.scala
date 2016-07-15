931d1356faa6eb540cc2ffaffefb2d9fe932647a
tverran <tom.verran@guardian.co.uk>
2016-07-15 14:12:42 +0100

    Membership common 238 & slug aware test users

--- app/controllers/Testing.scala
+++ app/controllers/Testing.scala
@@ -2,7 +2,9 @@ package controllers
 
 import actions.CommonActions._
 import com.typesafe.scalalogging.LazyLogging
+import model.Subscriptions.{SubscriptionOption, SubscriptionProduct}
 import play.api.mvc.{Controller, Cookie}
+import services.TouchpointBackend
 import utils.TestUsers.testUsers
 
 object Testing extends Controller with LazyLogging {
@@ -13,6 +15,8 @@ object Testing extends Controller with LazyLogging {
 
   val PreSigninTestCookieName = "pre-signin-test-user"
 
+  lazy val products = TouchpointBackend.Test.catalogService.paperCatalog.all
+
   def testUser = AuthorisedTester { implicit request =>
     val headers = request.headers
     logger.info(s"remoteAddress=${request.remoteAddress} $X_FORWARDED_FOR=${headers.getAll(X_FORWARDED_FOR).mkString("_")} $FORWARDED=${headers.getAll(FORWARDED).mkString("_")}")
@@ -20,7 +24,7 @@ object Testing extends Controller with LazyLogging {
     val testUserString = testUsers.generate()
     logger.info(s"Generated test user string $testUserString")
     val testUserCookie = new Cookie(PreSigninTestCookieName, testUserString, Some(30 * 60), httpOnly = true)
-    Ok(views.html.testing.testUsers(testUserString)).withCookies(testUserCookie, analyticsOffCookie)
+    Ok(views.html.testing.testUsers(testUserString, products)).withCookies(testUserCookie, analyticsOffCookie)
   }
 
   def analyticsOff = CachedAction {
--- build.sbt
+++ build.sbt
@@ -40,7 +40,7 @@ libraryDependencies ++= Seq(
     ws,
     filters,
     PlayImport.specs2,
-    "com.gu" %% "membership-common" % "0.237",
+    "com.gu" %% "membership-common" % "0.238",
     "com.gu" %% "memsub-common-play-auth" % "0.8",
     "com.gu" %% "content-authorisation-common" % "0.1",
     "com.github.nscala-time" %% "nscala-time" % "2.8.0",
