d72c01f497418f84811004ec34c143455307d0ae
Paul Brown <paul.brown@guardian.co.uk>
2016-07-25 19:31:36 +0100

    Added a redirect to the / and /digipack pages to do geo-ip resolution.

--- app/controllers/Homepage.scala
+++ app/controllers/Homepage.scala
@@ -1,18 +1,29 @@
 package controllers
 
-import actions.CommonActions.CachedAction
+import actions.CommonActions._
+import com.gu.i18n.CountryGroup
+import com.netaporter.uri.Uri
+import com.netaporter.uri.dsl._
 import model.DigitalEdition
+import model.DigitalEdition._
+import utils.RequestCountry._
 import play.api.mvc._
 
 object Homepage extends Controller {
 
-  def index = CachedAction(Ok(views.html.index()))
+  def index = NoCacheAction { implicit request =>
+    val countryGroup = request.getFastlyCountry.getOrElse(CountryGroup.UK)
+    val redirectUri = Uri.parse(request.uri) / getForCountryGroup(countryGroup).id
+    Redirect(request.toUriWithCampaignParams(redirectUri), SEE_OTHER)
+  }
 
-  def index(edition: DigitalEdition) = CachedAction(Ok(views.html.index_intl(edition)))
+  def indexUk = CachedAction(Ok(views.html.index()))
 
-  def indexAu() = index(DigitalEdition.AU)
+  def internationalHomepage(edition: DigitalEdition) = CachedAction(Ok(views.html.index_intl(edition)))
 
-  def indexUs() = index(DigitalEdition.US)
+  def indexAu = internationalHomepage(AU)
 
-  def indexInt() = index(DigitalEdition.INT)
+  def indexUs = internationalHomepage(US)
+
+  def indexInt = internationalHomepage(INT)
 }
