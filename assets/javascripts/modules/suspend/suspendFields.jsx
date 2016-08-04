ddf23ed3711ad171078fcd3383030844ba89f764
Paul Brown <paul.brown@guardian.co.uk>
2016-08-04 16:12:46 +0100

    Added the billing schedule to the suspend page. Also removed already-taken dates from the calendar so  the user won't accidentally pick them.

--- assets/javascripts/modules/suspend/suspendFields.jsx
+++ assets/javascripts/modules/suspend/suspendFields.jsx
@@ -11,16 +11,19 @@ require('react-datepicker/dist/react-datepicker.css');
 const FIRST_SELECTABLE_DATE = moment().businessAdd(5);
 const MAX_WEEKS = 3;
 
-function filterDate(packageName) {
+function filterDate(packageName, exclusionList) {
+    const exclusions = (exclusionList || '').split(',');
+    const isNotTaken = (date) => !exclusions.includes(date.format('D MMMM YYYY'));
+
     switch (true) {
         case /Sunday/i.test(packageName):
-            return (date) => date.day() === 0;
+            return (date) => date.day() === 0 && isNotTaken(date);
         case /Sixday/i.test(packageName):
-            return (date) => date.day() !== 0;
+            return (date) => date.day() !== 0 && isNotTaken(date);
         case /Weekend/i.test(packageName):
-            return (date) => date.day() === 6 || date.day() === 0;
+            return (date) => (date.day() === 6 || date.day() === 0) && isNotTaken(date);
         default:
-            return (date) => true;
+            return isNotTaken;
     }
 }
 
@@ -29,7 +32,7 @@ export default {
     renderDatePicker () {
         const container = document.getElementById(formElements.SUSPEND_DATE_PICKER_ID),
             remainingDays = parseInt(container.getAttribute('remainingDays'), 10),
-            filterDateFn = filterDate(container.getAttribute('ratePlanName'));
+            filterDateFn = filterDate(container.getAttribute('ratePlanName'), container.getAttribute('excludeExistingDays'));
 
         ReactDOM.render(
             <CustomDateRangePicker className="input-text"
