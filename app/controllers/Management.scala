a34cb425cf00e415b88440eff36507a3b2ca1069
tverran <tom.verran@guardian.co.uk>
2016-09-15 15:52:24 +0100

    Update healthcheck a bit

--- app/controllers/Management.scala
+++ app/controllers/Management.scala
@@ -3,7 +3,7 @@ package controllers
 import java.util.Date
 
 import app.BuildInfo
-import com.gu.memsub.services.CatalogService
+import scalaz.syntax.nel._
 import com.typesafe.scalalogging.LazyLogging
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
@@ -16,8 +16,9 @@ import play.api.Play.current
 import play.api.libs.concurrent.Akka
 import com.github.nscala_time.time.Imports._
 import com.gu.memsub.subsv2.Catalog
+import scalaz.syntax.applicative._
 import scalaz.syntax.std.option._
-import scalaz.\/
+import scalaz.{NonEmptyList, Validation, ValidationNel, \/}
 
 object Management extends Controller with LazyLogging {
   implicit val as = Akka.system
@@ -34,42 +35,42 @@ object Management extends Controller with LazyLogging {
   import TouchpointBackend.Normal._
 
   trait Test {
-    def ok: Boolean
-    def messages: Seq[String] = Nil
+    def run: ValidationNel[String, Unit]
   }
 
   class BoolTest(name: String, exec: () => Boolean) extends Test {
-    override def messages = List(s"Test $name failed, health check will fail")
-    override def ok = exec()
+    override def run = if(exec())
+      Validation.success(())
+    else
+      Validation.failureNel(s"Test $name failed, health check will fail")
   }
 
-  class EitherTest[A](name: String, exec: () => List[String] \/ A) extends Test {
-    override def messages = exec().swap.toOption.toList.flatten
-    override def ok: Boolean = exec().isRight
+  class EitherTest[A](name: String, exec: () => NonEmptyList[String] \/ A) extends Test {
+    override def run = exec().validation.map(_ => ())
   }
 
-  private def catalogTest: List[String] \/ Catalog = for {
-    catalogTry <- catalogService.catalog.value \/> List("Future not completed yet")
-    disjunction <- catalogTry.toOption \/> List(catalogTry.failed.map(_.getMessage).getOrElse(""))
-    catalog <- disjunction.leftMap(_.list)
+  private def catalogTest: NonEmptyList[String] \/ Catalog = for {
+    catalogTry <- catalogService.catalog.value \/> "Future not completed yet".wrapNel
+    catalogDisjunction <- catalogTry.toOption \/> catalogTry.failed.map(_.getMessage).getOrElse("").wrapNel
+    catalog <- catalogDisjunction
   } yield catalog
 
-  private def tests = Seq(
-    new BoolTest("CloudWatch", () => CloudWatchHealth.hasPushedMetricSuccessfully),
-    new BoolTest("ZuoraPing", () => zuoraService.lastPingTimeWithin(2.minutes)),
-    new BoolTest("Promotions", () => promoCollection.all.nonEmpty),
-    new EitherTest("CatalogSuccess", catalogTest _)
-  )
+  private def tests = (
+    new BoolTest("CloudWatch", () => CloudWatchHealth.hasPushedMetricSuccessfully).run |@|
+    new BoolTest("ZuoraPing", () => zuoraService.lastPingTimeWithin(2.minutes)).run |@|
+    new BoolTest("Promotions", () => promoCollection.all.nonEmpty).run |@|
+    new EitherTest("CatalogSuccess", catalogTest _).run
+  ).tupled
 
   def healthcheck() = Action {
     Cached(1) {
-      val failures = tests.filterNot(_.ok)
-      if (failures.isEmpty) {
-        Ok("OK")
-      } else {
-        failures.flatMap(_.messages).foreach(msg => warn(msg))
-        ServiceUnavailable("Service Unavailable")
-      }
+      tests.fold(
+        errors => {
+          errors.list.foreach(warn(_))
+          ServiceUnavailable("Service Unavailable")
+        },
+        _ => Ok("OK")
+      )
     }
   }
 
