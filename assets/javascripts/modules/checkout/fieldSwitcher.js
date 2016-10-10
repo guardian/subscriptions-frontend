8857a990ba5fb43a7dd5c4ce225b6dc4e0ef5b02
pvighi <patricio.vighi@guardian.co.uk>
2016-10-10 17:57:37 +0100

    refactored currency override code, probably needs more refactoring still

--- assets/javascripts/modules/checkout/fieldSwitcher.js
+++ assets/javascripts/modules/checkout/fieldSwitcher.js
@@ -9,22 +9,57 @@ define([
     'modules/checkout/ratePlanChoice'
 ], function ($, bean, countryChoiceFunction, addressFields, localizationSwitcher, formElements, ratePlanChoice) {
     'use strict';
+    var latestCurrencyAddressModel = null;
+    var check = function(domEl) {
+        $(domEl).attr('checked', 'checked');
+        bean.fire(domEl, 'change');
+    };
+    var checkPlanInput = function (ratePlanId, currency) {
+        ratePlanChoice.selectRatePlanForIdAndCurrency(ratePlanId, currency);
+    };
+
+    // Change the payment method to Direct Debit for UK users,
+    // Credit Card for international users
+    var selectPaymentMethod = function (country) {
+        if (country === 'GB') {
+            check(formElements.$DIRECT_DEBIT_TYPE[0]);
+        } else {
+            check(formElements.$CARD_TYPE[0]);
+        }
+    };
+
+    var switchLocalization = function (model) {
+        var currency = model.currency || guardian.currency;
+        localizationSwitcher.set(currency, model.country);
+    };
+
+    var switchCurrency = function(model) {
+        switchLocalization(model);
+        checkPlanInput(model.ratePlanId, model.currency);
+        selectPaymentMethod(model.country);
+    };
+    var initCurrencyOverride = function () {
+        //TODO SEE HOW TO GET THE DOM ELEMENT USING A $ SELECTOR AND CHECK IF I HAVE TO USE ONCLICK INSTEAD OF ONCHANGE FOR THE CHECKBOX
+        var currencyOverrideCheckbox = document.getElementById('currency-override-checkbox');
+        bean.on(currencyOverrideCheckbox, 'change', function () {
+            if (currencyOverrideCheckbox.checked) {
+                overrideCurrency('GBP');
+            }
+            else {
+                overrideCurrency('USD');
+            }
+        });
+    };
+    var overrideCurrency = function(currency) {
+        latestCurrencyAddressModel.currency = currency;
+        switchCurrency(latestCurrencyAddressModel);
+    };
 
     var everything = function(addressObject) {
         var $postcode = addressObject.$POSTCODE_CONTAINER,
             $subdivision = addressObject.$SUBDIVISION_CONTAINER,
             countryChoice = countryChoiceFunction(addressObject),
             shouldUpdateCurrency = addressObject.isCurrencyAddress();
-
-
-        var check = function(domEl) {
-            $(domEl).attr('checked', 'checked');
-            bean.fire(domEl, 'change');
-        };
-
-        var checkPlanInput = function (ratePlanId, currency) {
-            ratePlanChoice.selectRatePlanForIdAndCurrency(ratePlanId, currency);
-        };
 
         var redrawAddressField = function($container, newField, modelValue) {
 
@@ -57,58 +92,25 @@ define([
             redrawAddressField($subdivision, newSubdivision, model.subdivision);
         };
 
-        // Change the payment method to Direct Debit for UK users,
-        // Credit Card for international users
-        var selectPaymentMethod = function (country) {
-            if (country === 'GB') {
-                check(formElements.$DIRECT_DEBIT_TYPE[0]);
-            } else {
-                check(formElements.$CARD_TYPE[0]);
-            }
-        };
+        var redrawCountryOverride = function (selectedCurrency) {
+            $('#currency-override-checkbox').attr('checked', false);
+            //TODO FIGURE OUT WHY USING THIS LINE DOESNT WORK INSTEAD OF THE UGLY CODE BELOW
+            //$('#currency-override-label').toggle(selectedCurrency == 'USD')
 
-        var switchLocalization = function (model) {
-            var currency = model.currency || guardian.currency;
-            localizationSwitcher.set(currency, model.country);
-        };
-        var initCurrencyOverride = function () {
-            //TODO SEE HOW TO GET THE DOM ELEMENT USING A $ SELECTOR AND CHECK IF I HAVE TO USE ONCLICK INSTEAD OF ONCHANGE FOR THE CHECKBOX
-            var currencyOverrideCheckbox = document.getElementById('currency-override-checkbox');
-            bean.on(currencyOverrideCheckbox, 'change', function () {
-                if (currencyOverrideCheckbox.checked) {
-                    overrideCurrency('GBP');
-                }
-                else {
-                    overrideCurrency('USD');
-                }
-            });
-        };
-        var overrideCurrency = function(currency) {
-            var model = getCurrentState();
-            model.currency = currency;
-            switchCurrency(model);
-        };
-        var switchCurrency = function(model) {
-            switchLocalization(model);
-            checkPlanInput(model.ratePlanId, model.currency);
-            selectPaymentMethod(model.country);
-        };
-        var refreshCurrencyOverride = function (selectedCurrency) {
-            var checkbox = $('#currency-override-label');
+            var checkboxLabel = $('#currency-override-label');
             if (selectedCurrency == 'USD') {
-                checkbox.show();
+                checkboxLabel.show();
             } else {
-                checkbox.hide();
+                checkboxLabel.hide();
             }
         };
         var redraw = function(model) {
             redrawAddressFields(model);
             if (shouldUpdateCurrency) {
                 switchCurrency(model);
-                refreshCurrencyOverride(model.currency);
+                redrawCountryOverride(model.currency);
             }
         };
-
         var getCurrentState = function() {
             var currentCountryOption = $(countryChoice.getCurrentCountryOption()),
                 rules = countryChoice.addressRules();
@@ -134,6 +136,9 @@ define([
 
         var refresh = function () {
             var model = getCurrentState();
+            if (shouldUpdateCurrency) {
+                latestCurrencyAddressModel= model;
+            }
             redraw(model);
             updateGuardianPageInfo(model);
         };
@@ -162,8 +167,7 @@ define([
         init: function() {
             everything(formElements.BILLING).init();
             everything(formElements.DELIVERY).init();
-            //see what we can do about the useless parameter
-            everything(formElements.BILLING).initCurrencyOverride();
+            initCurrencyOverride();
 
         }
 
