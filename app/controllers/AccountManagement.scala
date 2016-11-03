7ef4cbf01925df5389af227797add725db92cac6
John Duffell <john.duffell@guardian.co.uk>
2016-11-03 16:48:41 +0000

    use the new readertype var to decide if we support weekly renewal

--- app/controllers/AccountManagement.scala
+++ app/controllers/AccountManagement.scala
@@ -110,10 +110,10 @@ object AccountManagement extends Controller with LazyLogging {
           Ok(views.html.account.voucher(voucherSubscription, billingSchedule))
         }
         case _: Product.Weekly => sub.asWeekly.map { weeklySubscription =>
-          if (weeklySubscription.gift)
-            Ok(views.html.account.details(None))// don't support gifts (yet) as they have related contacts in salesforce of unknown structure
-          else
+          if (weeklySubscription.readerType == ReaderType.Direct)
             Ok(views.html.account.renew(weeklySubscription, billingSchedule))
+          else
+            Ok(views.html.account.details(None))// don't support gifts (yet) as they have related contacts in salesforce of unknown structure
         }
       }).getOrElse {
         // the product type didn't have the right charges
