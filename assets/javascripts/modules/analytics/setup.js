1327e45da2ef395fdeb95529894acec348b79c81
Paul Brown <paul.brown@guardian.co.uk>
2016-11-17 18:23:23 +0000

    Remove all Omniture code, and the now redundant impressionTracking module.

--- assets/javascripts/modules/analytics/setup.js
+++ assets/javascripts/modules/analytics/setup.js
@@ -1,11 +1,9 @@
 define(['modules/analytics/ga',
-        'modules/analytics/omniture',
         'modules/analytics/ophan',
         'modules/analytics/remarketing',
         'modules/analytics/krux',
         'modules/analytics/affectv'
 ], function (ga,
-             omniture,
              ophan,
              remarketing,
              krux,
@@ -16,7 +14,6 @@ define(['modules/analytics/ga',
         ophan.init();
         // ga prefers ophan to have bootstrapped and set window.guardian state etc
         ophan.loaded.then(ga.init, ga.init);
-        omniture.init();
         remarketing.init();
         krux.init();
         affectv.init();
