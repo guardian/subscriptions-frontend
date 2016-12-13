9dfb6ac17622f195f5cce9f4c733b132599fe619
paul_brown <paul.brown@guardian.co.uk>
2016-12-13 14:15:56 +0000

    Final tweaks to only sell zone's A and C for Guardian Weekly Updating to lastest membership-common.

--- app/controllers/Checkout.scala
+++ app/controllers/Checkout.scala
@@ -56,12 +56,14 @@ object Checkout extends Controller with LazyLogging with CatalogProvider {
 
     val matchingPlanList: Option[PlanList[CatalogPlan.ContentSubscription]] = {
 
+      val testOnlyPlans = if (tpBackend == TouchpointBackend.Test) List(catalog.weeklyZoneB.toList) else List.empty
+
       val contentSubscriptionPlans = List(
         catalog.delivery.list,
         catalog.voucher.list,
         catalog.weeklyZoneA.toList,
-        catalog.weeklyZoneB.toList,
-        catalog.digipack.toList)
+        catalog.weeklyZoneC.toList,
+        catalog.digipack.toList) ++ testOnlyPlans
 
       def matchPlan(planCandidates: List[CatalogPlan.ContentSubscription]) = planCandidates.find(_.slug == forThisPlan).map(p => PlanList(p, getBetterPlans(p, planCandidates): _*))
 
