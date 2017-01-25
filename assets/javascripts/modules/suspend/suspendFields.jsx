c04507671402217616d4fe4a0e02d7e8a941fdb5
Paul Brown <paul.brown@guardian.co.uk>
2017-01-25 17:53:44 +0000

    Shorten the lead time to set a holiday suspension

--- assets/javascripts/modules/suspend/suspendFields.jsx
+++ assets/javascripts/modules/suspend/suspendFields.jsx
@@ -9,7 +9,7 @@ import moment from 'moment'
 require('react-datepicker/dist/react-datepicker.css');
 
 const DATE_PATTERN = 'D MMMM YYYY';
-const LEAD_TIME = moment().add(5, 'days');
+const LEAD_TIME = moment().add(2, 'days');
 const LAST_START_DATE = moment().add(1, 'year');
 const MAX_WEEKS = 6;
 
