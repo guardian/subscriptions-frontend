354f6a01e3f001240b31d5208981ed826f9d8ca3
Jacob Winch <jacob.winch@guardian.co.uk>
2017-03-22 14:27:04 +0000

    Fix implicits

--- app/controllers/AccountManagement.scala
+++ app/controllers/AccountManagement.scala
@@ -242,12 +242,12 @@ object ManageWeekly extends ContextLogging {
       } yield {
         info(s"Attempting to renew onto ${renew.plan.name} with promo code: ${renew.promoCode}")(sub)
         tpBackend.checkoutService.renewSubscription(renewableSub, renew).map { _ =>
-          info(s"Successfully processed renewal onto ${renew.plan.name}")
+          info(s"Successfully processed renewal onto ${renew.plan.name}")(sub)
           Ok(Json.obj("redirect" -> routes.AccountManagement.renewThankYou().url))
         }.recover{
             case e: Throwable =>
               val errorMessage = "Unexpected error while renewing subscription"
-              error(s"${errorMessage}: $e")
+              error(s"${errorMessage}: $e")(sub)
               InternalServerError(jsonError(errorMessage))
         }
       }
