5b0d21e4bc9c6ab176c03536e9999de5bfbb0f6c
Paul Brown <paul.brown@guardian.co.uk>
2017-05-05 14:05:52 +0100

    Better ordering of variables.

--- assets/javascripts/modules/analytics/appnexus.js
+++ assets/javascripts/modules/analytics/appnexus.js
@@ -26,6 +26,8 @@ define([
         if (!(guardian && guardian.pageInfo && guardian.pageInfo.productData && guardian.pageInfo.productData.productSegment)) { return; }
 
         var productSegment = guardian.pageInfo.productData.productSegment;
+        var pixelPath = (productSegment === 'Thankyou') ? 'px' : 'seg';
+        var parameterName = (productSegment === 'Thankyou') ? 'id' : 'add';
 
         var pageCodes = segmentedPageCodes[productSegment];
         if (!pageCodes) { return; }
@@ -33,11 +35,7 @@ define([
         var pageType = guardian.pageInfo.pageType;
         if (!pageType) { return; }
 
-        var parameterName = (productSegment === 'Thankyou') ? 'id' : 'add';
-
         var parameterValue = pageCodes[pageType];
-
-        var pixelPath = (productSegment === 'Thankyou') ? 'px' : 'seg';
 
         var oImg = document.createElement('img');
         oImg.setAttribute('src', 'https://secure.adnxs.com/' + pixelPath + '?t=2&' + parameterName + '=' + parameterValue);
