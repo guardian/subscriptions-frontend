bc52bc6cac5a421022fcfb3b7e9f6bb0445c83d6
Joseph Smith <joseph.smith@guardian.co.uk>
2017-10-26 17:01:20 +0100

    Store acquisitionData in cookie

--- app/actions/CommonActions.scala
+++ app/actions/CommonActions.scala
@@ -45,6 +45,12 @@ object CommonActions {
 
   val CachedAction = resultModifier(Cached(_))
 
+  val StoreAcquisitionDataAction = new ActionBuilder[Request] {
+    def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = block(request).map(result => {
+      request.getQueryString("acquisitionData").fold(result)(a => result.withSession(request.session.data.toSeq ++ Seq("acquisitionData" -> a) :_*))
+    })
+  }
+
   val CSRFCachedAsyncAction = (block: Request[_] => Future[Result]) => CSRFCheck(action = CachedAction.async(block), config = CSRFConfig.global.copy(checkContentType = (x: Option[String]) => true))
 
   def noCache(result: Result): Result = NoCache(result)
