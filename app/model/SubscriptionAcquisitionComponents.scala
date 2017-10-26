a9e51956814dd3695db0e803bad33bb85552e1ef
Joseph Smith <joseph.smith@guardian.co.uk>
2017-10-26 13:30:29 +0100

    Add all subs-related data

--- app/model/SubscriptionAcquisitionComponents.scala
+++ app/model/SubscriptionAcquisitionComponents.scala
@@ -2,7 +2,10 @@ package model
 
 import com.gu.acquisition.model.OphanIds
 import com.gu.acquisition.typeclasses.AcquisitionSubmissionBuilder
+import com.gu.memsub.BillingPeriod
+import com.gu.memsub.BillingPeriod.{Month, Quarter, SixMonths, Year}
 import com.gu.stripe.Stripe.Charge
+import ophan.thrift.event.PaymentProvider.{Gocardless, Stripe}
 import ophan.thrift.event.{Acquisition, PaymentFrequency, Product}
 
 case class SubscriptionAcquisitionComponents(subscribeRequest: SubscribeRequest)
@@ -15,30 +18,44 @@ object SubscriptionAcquisitionComponents {
 
     def buildOphanIds(components: SubscriptionAcquisitionComponents): Either[String, OphanIds] = {
       import components._
-      Right(OphanIds(pageviewId = "dummy", visitId = Some("dummy"), browserId = Some("dummy")))
+      Right(OphanIds(pageviewId = Some("dummy"), visitId = Some("dummy"), browserId = Some("dummy")))
     }
 
     override def buildAcquisition(components: SubscriptionAcquisitionComponents): Either[String, Acquisition] = {
       import components._
 
+      val plan = subscribeRequest.productData.fold(_.plan, _.plan)
+
       Right(
         Acquisition(
           product = subscribeRequest.productData match {
-            // TODO: THIS SHOULD BE PrintSubscription
-            case Left(_) => Product.PaperSubscriptionEveryday
+            case Left(_) => Product.PrintSubscription
             case Right(_) => Product.DigitalSubscription
           },
-          paymentFrequency = PaymentFrequency.Monthly,
-          currency = "dummy",
-          // Stripe amount is in smallest currency unit.
-          // Convert e.g. Pence to Pounds, Cents to Dollars
-          // https://stripe.com/docs/api#charge_object
-          amount = 12.5,
-          amountInGBP = None, // Calculated at the sinks of the Ophan stream
-          paymentProvider = None,
+
+          paymentFrequency = plan.charges.billingPeriod match {
+            case Month => PaymentFrequency.Monthly
+            case Quarter => PaymentFrequency.Quarterly
+            case SixMonths => PaymentFrequency.SixMonthly
+            case Year => PaymentFrequency.Annually
+          },
+
+          currency = subscribeRequest.genericData.currency.iso,
+
+          amount = plan.charges.price.getPrice(subscribeRequest.genericData.currency)
+            .map(_.amount.toDouble).getOrElse(0),
+
+          paymentProvider = subscribeRequest.genericData.paymentData match {
+            case DirectDebitData(_, _, _) => Some(Stripe)
+            case CreditCardData(_) => Some(Gocardless)
+            case _ => None
+          },
+
+          countryCode = subscribeRequest.genericData.personalData.address.country.map(_.alpha2),
+
+          // TODO: from request? session?
           campaignCode = None,
           abTests = None,
-          countryCode = None,
           referrerPageViewId = None,
           referrerUrl = None,
           componentId = None,
