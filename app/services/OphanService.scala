28e6df4200262e9f5732d726454a64a310460520
Joseph Smith <joseph.smith@guardian.co.uk>
2017-11-07 16:18:11 +0000

    Don't recreate ophan service on each request

--- app/services/OphanService.scala
+++ app/services/OphanService.scala
@@ -4,10 +4,12 @@ import com.gu.okhttp.RequestRunners
 
 object OphanService {
 
+  val prodService = com.gu.acquisition.services.OphanService.prod(RequestRunners.client)
+
   def apply(isTestService: Boolean): com.gu.acquisition.services.OphanService =
     if (isTestService) {
       com.gu.acquisition.services.MockOphanService
     } else {
-      com.gu.acquisition.services.OphanService.prod(RequestRunners.client)
+      prodService
     }
 }
