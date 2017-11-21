4b8a2deb53dc28a082cab3bb491dd090244bce9e
rupertbates <rupert.bates@guardian.co.uk>
2017-11-21 17:34:22 +0000

    International copy changes

--- app/model/promoCodes/PromoCode.scala
+++ app/model/promoCodes/PromoCode.scala
@@ -7,3 +7,5 @@ case object Digital extends PromoCodeKey
 case object Paper extends PromoCodeKey
 
 case object PaperAndDigital extends PromoCodeKey
+
+case object GuardianWeekly extends PromoCodeKey
--- app/services/BlackFriday.scala
+++ app/services/BlackFriday.scala
@@ -1,7 +1,8 @@
 package services
 
 import configuration.Config
-import model.promoCodes.{Digital, Paper, PaperAndDigital, PromoCodeKey}
+import model.DigitalEdition
+import model.promoCodes.{GuardianWeekly, _}
 import org.joda.time.DateTime
 
 //TODO: Remove after 3rd December 2017
@@ -12,35 +13,43 @@ object BlackFriday {
     val startTime = new DateTime(2017, 11, 24, 0, 0)
     val endTime = new DateTime(2017, 12, 4, 0, 0)
     val now = new DateTime()
-    now.isAfter(startTime) && now.isBefore(endTime) || !Config.stageProd //allow testing on CODE
+    now.isAfter(startTime) && now.isBefore(endTime) //|| !Config.stageProd //allow testing on CODE
   }
 
-  def homePromoCodes: Map[PromoCodeKey, String] = if (inOfferPeriod) {
+  def homePromoCodes: Map[PromoCodeKey, String] = homePromoCodes(DigitalEdition.UK)
+
+  def homePromoCodes(edition: DigitalEdition): Map[PromoCodeKey, String] = if (inOfferPeriod) {
     Map(
       Digital -> "DBQ80F",
       PaperAndDigital -> "GBQ80H",
-      Paper -> "GBQ80G"
+      Paper -> "GBQ80G",
+      GuardianWeekly -> s"WHOME${edition.id.toUpperCase}"
     )
   }
   else {
     Map(
-      Digital -> "DHOMEUK1",
-      PaperAndDigital -> "NHOMEUKD",
-      Paper -> "NHOMEUKP"
+      Digital -> s"DHOME${edition.id.toUpperCase}1",
+      PaperAndDigital -> s"NHOME${edition.id.toUpperCase}D",
+      Paper -> s"NHOME${edition.id.toUpperCase}P",
+      GuardianWeekly -> s"WHOME${edition.id.toUpperCase}"
     )
   }
 
-  def offersPromoCodes: Map[PromoCodeKey, String] = if (inOfferPeriod) {
+  def offersPromoCodes: Map[PromoCodeKey, String] = offersPromoCodes(DigitalEdition.UK)
+
+  def offersPromoCodes(edition: DigitalEdition): Map[PromoCodeKey, String] = if (inOfferPeriod) {
     Map(
       Digital -> "DBQ80J",
       PaperAndDigital -> "GBQ80L",
-      Paper -> "GBQ80K"
+      Paper -> "GBQ80K",
+      GuardianWeekly -> "WAL41X"
     )
   } else {
     Map(
-      Digital -> "DOFFUK1",
-      PaperAndDigital -> "NOFFUKD",
-      Paper -> "NOFFUKP"
+      Digital -> s"DOFF${edition.id.toUpperCase}1",
+      PaperAndDigital -> s"NOFF${edition.id.toUpperCase}D",
+      Paper -> s"NOFF${edition.id.toUpperCase}P",
+      GuardianWeekly -> "WAL41X"
     )
   }
 }
