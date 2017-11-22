830724109ffdff464db62ed3a9aa42b4453d9d83
rupertbates <rupert.bates@guardian.co.uk>
2017-11-22 12:04:43 +0000

    Extract the digital pack section into a separate fragment
    To reduce code duplication

--- app/views/support/DigitalEdition.scala
+++ app/views/support/DigitalEdition.scala
@@ -1,6 +1,7 @@
 package views.support
 import com.netaporter.uri.Uri
 import com.netaporter.uri.dsl._
+import controllers.routes
 import model.DigitalEdition._
 import model.{DigitalEdition => DE}
 import utils.Tracking.internalCampaignCode
@@ -26,6 +27,11 @@ object DigitalEdition {
         case _ => "https://membership.theguardian.com/join" ? params
       }
     }
+
+    def getDestinationUrl(promoCode: String, includeEdition: Boolean) = if(includeEdition)
+      routes.PromoLandingPage.render(promoCode, None).url + "?edition=" + edition.id
+    else
+      routes.PromoLandingPage.render(promoCode, edition.countryGroup.defaultCountry).url
   }
 
 }
