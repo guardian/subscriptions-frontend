9a9f33aad1d6db328641f23559c6d82d3e3ad782
Jacob Winch <jacob.winch@guardian.co.uk>
2018-02-15 11:44:23 +0000

    Don't use fulfilment-lookup response body

--- app/services/FulfilmentLookupService.scala
+++ app/services/FulfilmentLookupService.scala
@@ -4,10 +4,9 @@ import com.gu.okhttp.RequestRunners.futureRunner
 import com.typesafe.scalalogging.StrictLogging
 import configuration.Config
 import forms.ReportDeliveryProblem
-import model.FulfilmentLookup
 import okhttp3.{MediaType, RequestBody}
 import org.joda.time.format.DateTimeFormat
-import play.api.libs.json.{JsError, JsSuccess, Json}
+import play.api.libs.json.Json
 import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scalaz.{-\/, \/, \/-}
@@ -32,7 +31,7 @@ object FulfilmentLookupService extends StrictLogging {
       .build()
   }
 
-  def lookupSubscription(env: String, deliveryProblem: ReportDeliveryProblem): Future[String \/ FulfilmentLookup] = {
+  def lookupSubscription(env: String, deliveryProblem: ReportDeliveryProblem): Future[String \/ Unit] = {
     val request = buildRequest(env, deliveryProblem)
     val futureResponse = httpClient(request)
     futureResponse.map { response =>
@@ -40,13 +39,7 @@ object FulfilmentLookupService extends StrictLogging {
       response.body.close
       if (response.isSuccessful) {
         logger.info(s"[${env}] Successfully performed lookup for ${deliveryProblem.subscriptionName}")
-        val jsonBody = Json.parse(responseBody)
-        jsonBody.validate[FulfilmentLookup] match {
-          case validLookup: JsSuccess[FulfilmentLookup] =>
-            \/-(validLookup.value)
-          case error: JsError =>
-            -\/(s"response was successful but body could not be parsed, we got: $responseBody")
-        }
+        \/-(())
       } else {
         -\/(s"response was unsuccessful, we got a ${response.code}: ${responseBody}")
       }
