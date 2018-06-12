ff5d0cdc433c3fce711df2239bec1bcab46ef45e
rupertbates <rupert.bates@guardian.co.uk>
2018-06-12 16:22:41 +0100

    New dates and promo codes

--- app/services/FlashSale.scala
+++ app/services/FlashSale.scala
@@ -8,16 +8,16 @@ import org.joda.time.DateTime
 object FlashSale {
 
   def inOfferPeriod(promoCodeKey: PromoCodeKey) = {
-    //The offer is valid between 15th May 2018 & 29th May 2018
-    //The current sale is paper & paper + digital only, digital is unaffected
+    //The offer is valid between 18th June 2018 & 1st July 2018
+    //The current sale is digital only, paper & paper + digital are unaffected
     val included: Map[PromoCodeKey, Boolean] = Map(
-      Digital -> false,
-      Paper -> true,
-      PaperAndDigital -> true
+      Digital -> true,
+      Paper -> false,
+      PaperAndDigital -> false
     )
 
-    val startTime = new DateTime(2018, 5, 15, 0, 0)
-    val endTime = new DateTime(2018, 5, 29, 0, 0)
+    val startTime = new DateTime(2018, 6, 18, 0, 0)
+    val endTime = new DateTime(2018, 7, 1, 0, 0)
     val now = new DateTime()
 
     now.isAfter(startTime) &&
@@ -30,7 +30,7 @@ object FlashSale {
 
   def homePromoCodes(edition: DigitalEdition): Map[PromoCodeKey, String] =
     Map(
-      Digital -> getCode(Digital, s"DHOME${edition.id.toUpperCase}1", "DBR80F"),
+      Digital -> getCode(Digital, s"DHOME${edition.id.toUpperCase}1", "DPS80P"),
       PaperAndDigital -> getCode(PaperAndDigital, s"NHOME${edition.id.toUpperCase}D", "GST80K"),
       Paper -> getCode(Paper, s"NHOME${edition.id.toUpperCase}P", "GST80J"),
       GuardianWeekly -> s"WHOME${edition.id.toUpperCase}"
@@ -40,7 +40,7 @@ object FlashSale {
 
   def offersPromoCodes(edition: DigitalEdition): Map[PromoCodeKey, String] =
     Map(
-      Digital -> getCode(Digital, s"DOFF${edition.id.toUpperCase}1", "DBR80G"),
+      Digital -> getCode(Digital, s"DOFF${edition.id.toUpperCase}1", "DPS80P"),
       PaperAndDigital -> getCode(PaperAndDigital, s"NOFF${edition.id.toUpperCase}D", "GST80I"),
       Paper -> getCode(Paper, s"NOFF${edition.id.toUpperCase}P", "GST80H"),
       GuardianWeekly -> "WAL41X"
