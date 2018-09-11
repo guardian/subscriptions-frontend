561e37c32120d9d648cf694b115269a098bc76f3
Peter Colley <zvvvvt@gmail.com>
2018-09-11 12:03:36 +0100

    don't send identity id to sqs queue for email send. It resolved in membership workflow

--- app/model/PurchaserIdentifiers.scala
+++ app/model/PurchaserIdentifiers.scala
@@ -6,5 +6,4 @@ import com.gu.salesforce.ContactId
 case class PurchaserIdentifiers(contactId: ContactId, identityId: Option[IdMinimalUser]) {
   val description = (Seq(s"SalesforceContactID - ${contactId.salesforceContactId}") ++ identityId.map(id => s"IdentityID - $id")).mkString(" ")
   override def toString: String = description
-  lazy val identityIdWithFallback = identityId.map(_.id).getOrElse(contactId.salesforceContactId)
 }
