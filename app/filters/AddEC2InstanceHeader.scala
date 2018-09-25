12fd9aeeb98c79d8cc20c4f1225365e80eda7ce1
Jacob Winch <jacob.winch@guardian.co.uk>
2018-09-25 15:35:42 +0100

    Fix slow startup

--- app/filters/AddEC2InstanceHeader.scala
+++ app/filters/AddEC2InstanceHeader.scala
@@ -1,5 +1,6 @@
 package filters
 
+import configuration.Config
 import akka.stream.Materializer
 import play.api.libs.ws.WSClient
 import play.api.mvc._
@@ -9,7 +10,10 @@ import scala.concurrent.{ExecutionContext, Future}
 class AddEC2InstanceHeader (wSClient: WSClient)(implicit val mat: Materializer, val executionContext: ExecutionContext) extends Filter {
 
   // http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
-  lazy val instanceIdOptF = wSClient.url("http://169.254.169.254/latest/meta-data/instance-id").get().map(resp => Some(resp.body).filter(_.nonEmpty)).recover { case _ => None }
+  lazy val instanceIdOptF = if (Config.stage == "DEV")
+    Future.successful(None)
+  else
+    wSClient.url("http://169.254.169.254/latest/meta-data/instance-id").get().map(resp => Some(resp.body).filter(_.nonEmpty)).recover { case _ => None }
 
   def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = for {
     result <- nextFilter(requestHeader)
