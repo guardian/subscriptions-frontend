cb28cdee40c9a62090348686b2862ba2fb768c72
Paul Brown <paul.brown@guardian.co.uk>
2018-12-14 13:49:27 +0000

    Some code review improvements to https://github.com/guardian/subscriptions-frontend/pull/1222

--- app/services/SalesforceService.scala
+++ app/services/SalesforceService.scala
@@ -18,18 +18,19 @@ trait SalesforceService extends LazyLogging {
 }
 
 class SalesforceServiceImp(val repo: SimpleContactRepository) extends SalesforceService {
+  val recordTypeId = repo.recordTypes.getIdForContactRecordType(DeliveryRecipientContact)
+
+  private def createRecipientContactIfGift(buyerContact: ContactId, paperData: Option[PaperData]): Option[Future[ContactId]] = {
+    // Only create a Delivery / Recipient (Related) Contact if the recipient has a giftee address
+    paperData.map(_.deliveryRecipient).filter(_.gifteeAddress.nonEmpty).map { recipient =>
+      repo.upsert(None, SalesforceService.getJSONForRecipientContact(buyerContact, recipient, recordTypeId))
+    }
+  }
+
   override def createOrUpdateBuyerAndRecipient(personalData: PersonalData, paperData: Option[PaperData], userId: Option[IdMinimalUser]): Future[PurchaserIdentifiers] = {
     for {
       buyerContact <- repo.upsert(userId.map(_.id), SalesforceService.getJSONForBuyerContact(personalData, paperData))
-      recipientContact <- { // Only create a Delivery / Recipient (Related) Contact if the recipient is a Giftee
-        val gifteeRecipient = paperData.map(_.deliveryRecipient).filter(_.isGiftee)
-        gifteeRecipient.map { recipient =>
-          val recordTypeId = repo.recordTypes.getIdForContactRecordType(DeliveryRecipientContact)
-          repo.upsert(None, SalesforceService.getJSONForRecipientContact(buyerContact, recipient, recordTypeId))
-        } getOrElse {
-          Future.successful(buyerContact)
-        }
-      }
+      recipientContact <- createRecipientContactIfGift(buyerContact, paperData) getOrElse Future.successful(buyerContact)
     } yield PurchaserIdentifiers(buyerContact, recipientContact, userId)
   }
 
@@ -49,7 +50,7 @@ object SalesforceService {
     Keys.ALLOW_GU_RELATED_MAIL -> personalData.receiveGnmMarketing
   ) ++ personalData.title.fold(Json.obj())(title => Json.obj(
     Keys.TITLE -> title.title
-  )) ++ paperData.map(_.deliveryRecipient).filterNot(_.isGiftee).map(_.address).fold(Json.obj())(addr => Json.obj(
+  )) ++ paperData.flatMap(_.deliveryRecipient.buyersMailingAddress).fold(Json.obj())(addr => Json.obj(
     Keys.MAILING_STREET -> addr.line,
     Keys.MAILING_CITY -> addr.town,
     Keys.MAILING_POSTCODE -> addr.postCode,
@@ -65,13 +66,14 @@ object SalesforceService {
     Keys.EMAIL -> deliveryRecipient.email,
     Keys.TITLE -> deliveryRecipient.title.map(_.title).mkString,
     Keys.FIRST_NAME -> deliveryRecipient.first,
-    Keys.LAST_NAME -> deliveryRecipient.last,
-    Keys.MAILING_STREET -> deliveryRecipient.address.line,
-    Keys.MAILING_CITY -> deliveryRecipient.address.town,
-    Keys.MAILING_POSTCODE -> deliveryRecipient.address.postCode,
-    Keys.MAILING_STATE -> deliveryRecipient.address.countyOrState,
-    Keys.MAILING_COUNTRY -> deliveryRecipient.address.country.fold(deliveryRecipient.address.countryName)(_.name)
-  )
+    Keys.LAST_NAME -> deliveryRecipient.last
+  ) ++ deliveryRecipient.gifteeAddress.fold(Json.obj())(addr => Json.obj(
+    Keys.MAILING_STREET -> addr.line,
+    Keys.MAILING_CITY -> addr.town,
+    Keys.MAILING_POSTCODE -> addr.postCode,
+    Keys.MAILING_STATE -> addr.countyOrState,
+    Keys.MAILING_COUNTRY -> addr.country.fold(addr.countryName)(_.name)
+  ))
 }
 
 case class SalesforceServiceError(s: String) extends Throwable {
