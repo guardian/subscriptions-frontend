fb17bc8fb391e3ef3e4875c3decf4b3eec072592
rupertbates <rupert.bates@guardian.co.uk>
2019-02-04 17:57:12 +0000

    If an Optimize experiment query param is present, store it in the acquisitionData

--- app/actions/CommonActions.scala
+++ app/actions/CommonActions.scala
@@ -1,21 +1,27 @@
 package actions
 
- import controllers.{Cached, NoCache}
- import play.api.mvc._
- import play.filters.csrf.CSRFCheck
+import acquisitions.AcquisitionsHelper
+import controllers.{Cached, NoCache}
+import play.api.mvc._
+import play.filters.csrf.CSRFCheck
 
- import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future}
+
 final class CommonActions(executionContext: ExecutionContext, cSRFCheck: CSRFCheck, parser: BodyParser[AnyContent]) {
 
   private implicit val executionContextImplicit = executionContext
 
   val StoreAcquisitionDataAction = new ActionBuilder[Request, AnyContent] {
     def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = block(request).map(result => {
-      request.getQueryString("acquisitionData").fold(result)(a => {
+      AcquisitionsHelper.mergeAcquisitionDataJson(
+        request.getQueryString("acquisitionData"),
+        request.getQueryString("utm_expid")
+      ).fold(result)(a => {
         val sessionWithAcquisitionData = result.session(request).data.toSeq ++ Seq("acquisitionData" -> a)
         result.withSession(sessionWithAcquisitionData: _*)
       })
     })
+
     override def parser = CommonActions.this.parser
 
     override protected def executionContext: ExecutionContext = CommonActions.this.executionContext
