d9374d3e8460416c362cf1e7bd2ac942d1c080f7
Mullefa <gccdawson@gmail.com>
2019-07-30 12:43:24 +0100

    remove commented out code

--- test/utils/TestIdUser.scala
+++ test/utils/TestIdUser.scala
@@ -1,14 +1,8 @@
 package utils
 
-import com.gu.identity.model.{PrivateFields, PublicFields, StatusFields, TelephoneNumber, User => IdUser}
-import play.api.libs.json._
+import com.gu.identity.model.{PrivateFields, PublicFields, StatusFields, User => IdUser}
 
 object TestIdUser {
-//  implicit val writesStatusFields = Json.writes[StatusFields]
-//  implicit val writesTelephoneNumber = Json.writes[TelephoneNumber]
-//  implicit val writesPrivateFields = Json.writes[PrivateFields]
-//  implicit val writesPublicFields = Json.writes[PublicFields]
-//  implicit val writesIdUser = Json.writes[IdUser]
 
   val testUser = IdUser(
     id = "test-user",
