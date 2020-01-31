07e8d76acf27d40b49640d1ffc104b41b139aa35
Francis Rhys-Jones <francis@rhys-jones.com>
2020-01-31 16:34:17 +0000

    Pushing users to manage plaform for management of holiday stops
    Until the backfill/migration of legacy holiday stops to the new system, they will still show up on the delivery management page.

---
+++ app/utils/ManageUrlBuilder.scala
@@ -0,0 +1,23 @@
+package utils
+
+import com.gu.memsub.Product
+
+object ManageUrlBuilder {
+  def paymentUrl(baseUrl: String, product: Product) = {
+    productSpecificUrl(baseUrl, "payment", product)
+  }
+
+  def suspendUrl(baseUrl: String, product: Product) = {
+      productSpecificUrl(baseUrl, "suspend", product)
+  }
+
+  private def productSpecificUrl(baseUrl: String, path: String, product: Product )= {
+    product match {
+      case Product.Voucher => s"$baseUrl/$path/voucher"
+      case Product.Delivery => s"$baseUrl/$path/delivery"
+      case _: Product.ZDigipack => s"$baseUrl/$path/digitalpack"
+      case _: Product.Weekly => s"$baseUrl/$path/guardianweekly"
+      case _ => ""
+    }
+  }
+}
