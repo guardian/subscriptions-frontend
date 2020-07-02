4e778d3132eb1549deaee312dd8c264cc3fdeeab
Paul Brown <paulbrown1982@yahoo.co.uk>
2020-07-02 12:54:17 +0100

    Removed all 3rd party trackers/tags, but left a stub area for if we want to add anymore. For now, GA is loaded as a 1st party tag, to be consistent with other properties. I've refactored how it loads after after Ophan to be more robust around honouring the DNT and DEV analytics configs. Fixed a bug with `yarn watch` command so it compiles the JS to the --dev version of the code, so that `grunt watch --dev` also works.

--- assets/javascripts/modules/analytics/ophan.js
+++ assets/javascripts/modules/analytics/ophan.js
@@ -1,19 +1,21 @@
 define([
+    'modules/analytics/analyticsEnabled',
     'modules/raven'
-],function(raven) {
+],function(analyticsEnabled, raven) {
     'use strict';
 
     var ophanUrl = '//j.ophan.co.uk/membership.js';
-    var ophan = curl(ophanUrl);
 
     function init() {
-        ophan.then(null, function(err) {
+        return curl(ophanUrl).then(null, function(err) {
             raven.Raven.captureException(err);
         });
     }
 
     return {
-        init: init,
-        loaded: ophan // curl promise
+        init: analyticsEnabled(
+            function() { return Promise.resolve(init()); }, // init is 'thenable', not a true Promise, so needs wrapping
+            function() { return Promise.reject('Ophan not loaded due to analytics disabled') }
+        )
     };
 });
