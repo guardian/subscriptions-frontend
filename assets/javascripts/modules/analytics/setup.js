95145451c367bef1b5466f7150c10cc87c7b364e
George Haberis <george.haberis@guardian.co.uk>
2020-08-11 14:46:20 +0100

    refactor

--- assets/javascripts/modules/analytics/setup.js
+++ assets/javascripts/modules/analytics/setup.js
@@ -1,21 +1,28 @@
-define(['modules/analytics/ga',
-        'modules/analytics/ophan',
-        // 'modules/analytics/thirdPartyTracking'
-], function (ga,
-             ophan,
-            //  thirdPartyTracking
-             ) {
+define([
+    'modules/analytics/ga',
+    'modules/analytics/ophan',
+    'modules/analytics/thirdPartyTracking'
+], function (
+    ga,
+    ophan,
+    thirdPartyTracking
+) {
     'use strict';
 
     function init() {
         ophan.init();
 
-        // GA prefers Ophan to have bootstrapped and set window.guardian state, so load it after Ophan has loaded
-        ophan.loaded.finally(ga.init);
-
-        // thirdPartyTracking.thirdPartyTrackingEnabled().then(thirdPartyTrackingEnabled => {
-            // As this site is mostly deprecated, we've removed all 3rd party trackers.
-        // });
+        /**
+         * GA prefers Ophan to have bootstrapped and set window.guardian state,
+         * so load it after Ophan has loaded if thirdPartyTrackingEnabled.
+        */
+        ophan.loaded.finally(() => {
+            thirdPartyTracking.thirdPartyTrackingEnabled().then(thirdPartyTrackingEnabled => {
+                if (thirdPartyTrackingEnabled) {
+                    ga.init();
+                }
+            });
+        });
     }
 
     return {
