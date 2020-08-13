bbf0cbea2eb87258f993eb1dec531849600e4c2c
Paul Brown <paulbrown1982@yahoo.co.uk>
2020-08-13 18:06:44 +0100

    Fixed issue with double-callback after user makes a consent decision.

--- assets/javascripts/modules/analytics/setup.js
+++ assets/javascripts/modules/analytics/setup.js
@@ -9,6 +9,29 @@ define([
 ) {
     'use strict';
 
+    function loadGA() {
+        Promise.allSettled([
+            cmp.checkCCPA(),
+            cmp.getConsentForVendors([ga.cmpVendorId]),
+            cmp.checkAllTCFv2PurposesAreOptedIn()
+        ]).then(results => {
+            const [ ccpaConsent, vendorConsents, allPurposesAgreed ] = results.map(promise => promise.value);
+
+            if (ccpaConsent || vendorConsents[ga.cmpVendorId]) {
+                ga.init();
+                loadGA.complete = true;
+            } else {
+                if (ccpaConsent === null) {
+                    console.log('Either there\'s insufficient consent for Google Analytics, or the user has ' +
+                        `turned that vendor off in the CMP (${ga.cmpVendorId}). ` +
+                        `The user has ${allPurposesAgreed ? '' : 'not '}agreed to all purposes.`);
+                } else {
+                    console.log(`Google Analytics (${ga.cmpVendorId}) not loaded due to CCPA opt-out`);
+                }
+            }
+        });
+    }
+
     function init() {
         ophan.init();
 
@@ -16,27 +39,7 @@ define([
          * GA and other tags often prefer Ophan to have bootstrapped and set window.guardian state,
          * so load them after Ophan has loaded if thirdPartyTrackingEnabled.
          */
-        ophan.loaded.finally(() => {
-            Promise.allSettled([
-                cmp.checkCCPA(),
-                cmp.getConsentForVendors([ga.cmpVendorId]),
-                cmp.checkAllTCFv2PurposesAreOptedIn()
-            ]).then(results => {
-                const [ ccpaConsent, vendorConsents, allPurposesAgreed ] = results.map(promise => promise.value);
-
-                if (ccpaConsent || vendorConsents[ga.cmpVendorId]) {
-                    ga.init();
-                } else {
-                    if (ccpaConsent === null) {
-                        console.log('Either there\'s insufficient consent for Google Analytics, or the user has ' +
-                            `turned that vendor off in the CMP (${ga.cmpVendorId}). ` +
-                            `The user has ${allPurposesAgreed ? '' : 'not'} agreed to all purposes.`);
-                    } else {
-                        console.log(`Google Analytics (${ga.cmpVendorId}) not loaded due to CCPA opt-out`);
-                    }
-                }
-            });
-        });
+        ophan.loaded.finally(() => cmp.registerCallbackOnConsentChange(loadGA));
     }
 
     return {
